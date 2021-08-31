ae919ab3971f743827088075ea5fe0d660bc0e9e
Dorukyum <doruk.ak@hotmail.com>
2021-08-31 17:50:13 +0300

    Typehint discord/bot.py

--- discord/bot.py
+++ discord/bot.py
@@ -22,24 +22,25 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-from __future__ import annotations # will probably need in future for type hinting
+from __future__ import annotations
 
-import asyncio
-import inspect
-from typing import Callable
+from typing import Callable, Dict, List, Optional
 
+from .app import (ApplicationCommand, MessageCommand, SlashCommand,
+                  SubCommandGroup, UserCommand)
 from .client import Client
+from .interactions import Interaction
 from .shard import AutoShardedClient
 from .utils import get
-from .app import SlashCommand, SubCommandGroup, MessageCommand, UserCommand, ApplicationCommand
+
 
 class ApplicationCommandMixin:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.to_register = []
-        self.app_commands = {}
+        self.to_register: List[ApplicationCommand] = []
+        self.app_commands: Dict[str, ApplicationCommand] = {}
 
-    def add_application_command(self, command):
+    def add_application_command(self, command: ApplicationCommand) -> None:
         """Adds a :class:`.ApplicationCommand` into the internal list of commands.
 
         This is usually not called, instead the :meth:`~.ApplicationMixin.command` or
@@ -54,7 +55,9 @@ class ApplicationCommandMixin:
         """
         self.to_register.append(command)
 
-    def remove_application_command(self, command):
+    def remove_application_command(
+        self, command: ApplicationCommand
+    ) -> Optional[ApplicationCommand]:
         """Remove a :class:`.ApplicationCommand` from the internal list
         of commands.
 
@@ -73,7 +76,7 @@ class ApplicationCommandMixin:
         """
         return self.app_commands.pop(command.id)
 
-    async def sync_commands(self):
+    async def sync_commands(self) -> None:
         """
         pending removal
         """
@@ -86,10 +89,12 @@ class ApplicationCommandMixin:
         new_cmds = {}
         self.app_commands = {}
         for i in cmds:
-            cmd = get(to_add, name=i["name"], description=i["description"], type=i["type"])
+            cmd = get(
+                to_add, name=i["name"], description=i["description"], type=i["type"]
+            )
             new_cmds[i["id"]] = cmd
 
-    async def register_commands(self):
+    async def register_commands(self) -> None:
         """|coro|
 
         Registers all commands that have been added through :meth:`.ApplicationCommandMixin.add_application_command`.
@@ -107,10 +112,14 @@ class ApplicationCommandMixin:
         for command in [cmd for cmd in self.to_register if cmd.guild_ids is None]:
             as_dict = command.to_dict()
             if len(registered_commands) > 0:
-                matches = [x for x in registered_commands if x["name"] == command.name and x['type'] == command.type]
+                matches = [
+                    x
+                    for x in registered_commands
+                    if x["name"] == command.name and x["type"] == command.type
+                ]
                 # TODO: rewrite this, it seems inefficient
-                if len(matches) > 0:
-                    as_dict['id'] = matches[0]["id"]
+                if matches:
+                    as_dict["id"] = matches[0]["id"]
             commands.append(as_dict)
 
         update_guild_commands = {}
@@ -122,22 +131,32 @@ class ApplicationCommandMixin:
                 to_update = update_guild_commands[guild_id]
                 update_guild_commands[guild_id] = to_update + [as_dict]
 
-        for guild_id in update_guild_commands:
-            if update_guild_commands[guild_id]:
-                cmds = await self.http.bulk_upsert_guild_commands(self.user.id, guild_id, update_guild_commands[guild_id])
+        for guild_id, value in update_guild_commands.items():
+            if value:
+                cmds = await self.http.bulk_upsert_guild_commands(
+                    self.user.id, guild_id, update_guild_commands[guild_id]
+                )
                 for i in cmds:
-                    cmd = get(self.to_register, name=i["name"], description=i["description"], type=i['type'])
+                    cmd = get(
+                        self.to_register,
+                        name=i["name"],
+                        description=i["description"],
+                        type=i["type"],
+                    )
                     self.app_commands[i["id"]] = cmd
 
         cmds = await self.http.bulk_upsert_global_commands(self.user.id, commands)
 
         for i in cmds:
             cmd = get(
-                self.to_register, name=i["name"], description=i["description"], type=i['type']
+                self.to_register,
+                name=i["name"],
+                description=i["description"],
+                type=i["type"],
             )
             self.app_commands[i["id"]] = cmd
 
-    async def handle_interaction(self, interaction):
+    async def handle_interaction(self, interaction: Interaction) -> None:
         """|coro|
 
         This function processes the commands that have been registered
@@ -167,13 +186,13 @@ class ApplicationCommandMixin:
         else:
             await command.invoke(interaction)
 
-    def slash_command(self, **kwargs):
+    def slash_command(self, **kwargs) -> SlashCommand:
         return self._command_wrapper(SlashCommand, **kwargs)
 
-    def user_command(self, **kwargs):
+    def user_command(self, **kwargs) -> UserCommand:
         return self._command_wrapper(UserCommand, **kwargs)
-    
-    def message_command(self, **kwargs):
+
+    def message_command(self, **kwargs) -> MessageCommand:
         return self._command_wrapper(MessageCommand, **kwargs)
 
     def _command_wrapper(self, cls, **kwargs):
@@ -181,12 +200,15 @@ class ApplicationCommandMixin:
             if isinstance(func, (SlashCommand, UserCommand)):
                 func = func.callback
             elif not callable(func):
-                raise TypeError("func needs to be a callable, SlashCommand, or UserCommand object.")
+                raise TypeError(
+                    "func needs to be a callable, SlashCommand, or UserCommand object."
+                )
 
             command = cls(func, **kwargs)
             self.add_application_command(command)
             return command
-        return wrap        
+
+        return wrap
 
     def command(self, type=SlashCommand, **kwargs):
         if not issubclass(type, ApplicationCommand):
@@ -198,27 +220,33 @@ class ApplicationCommandMixin:
         elif type.type == 3:
             return self.message_command(**kwargs)
         else:
-            raise TypeError("type must be one of SlashCommand, UserCommand, MessageCommand")
+            raise TypeError(
+                "type must be one of SlashCommand, UserCommand, MessageCommand"
+            )
 
-    def command_group(self, name, description, guild_ids=None):
+    def command_group(
+        self, name: str, description: str, guild_ids=None
+    ) -> SubCommandGroup:
         group = SubCommandGroup(name, description, guild_ids)
         self.add_application_command(group)
         return group
+
 
 class BotBase(ApplicationCommandMixin):  # To Insert: CogMixin
     # TODO I think
     # def __init__(self, *args, **kwargs):
     #     super(Client, self).__init__(*args, **kwargs)
 
-    async def on_connect(self):
+    async def on_connect(self) -> None:
         await self.register_commands()
 
-    async def on_interaction(self, interaction):
+    async def on_interaction(self, interaction: Interaction) -> None:
         await self.handle_interaction(interaction)
 
 
 class Bot(BotBase, Client):
     pass
+
 
 class AutoShardedBot(BotBase, AutoShardedClient):
     pass
