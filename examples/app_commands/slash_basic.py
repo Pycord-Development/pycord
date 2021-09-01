c8d76a7c06018da19a80d021f12ab20e29fa00f3
unknown <codewithswastik@gmail.com>
2021-09-01 20:53:47 +0530

    Add another example in slash_basic

--- examples/app_commands/slash_basic.py
+++ examples/app_commands/slash_basic.py
@@ -1,15 +1,23 @@
 import discord
 
-bot = discord.Bot()
+bot = discord.Bot() 
+
+# If you use commands.Bot, @bot.slash_command should be used for 
+# slash commands. You can use @bot.slash_command with discord.Bot aswell
 
 
 @bot.command(guild_ids=[...])  # create a slash command for the supplied guilds
 async def hello(ctx):
     """Say hello to the bot""" # the command description can be supplied as the docstring
-    await ctx.respond(f"Hello {ctx.author}!")
+    await ctx.send(f"Hello {ctx.author}!")
 
-@bot.command(name='global')  # creates a global slash command (might take an hour to register)
+@bot.command(name='hi')  # Not passing in guild_ids creates a global slash command (might take an hour to register)
 async def global_command(ctx, num: int): # Takes one integer parameter 
-    await ctx.respond(f"This is a global command, {num}!")
+    await ctx.send(f"This is a global command, {num}!")
+
+@bot.command(guild_ids=[...])
+async def joined(ctx, member: discord.Member = None): # Passing a default value makes the argument optional
+    user = member or ctx.author 
+    await ctx.send(f'{user.name} joined at {discord.utils.format_dt(user.joined_at)}')
 
 bot.run("TOKEN")
