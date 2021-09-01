1453c22174ab2feb9abd81b0f9b3599cf03e2813
unknown <codewithswastik@gmail.com>
2021-09-01 11:06:17 +0530

    Improve examples

--- examples/app_commands/slash-basic.py
+++ examples/app_commands/slash-basic.py
@@ -6,7 +6,10 @@ bot = discord.Bot()
 @bot.command(guild_ids=[...])  # create a slash command for the supplied guilds
 async def hello(ctx):
     """Say hello to the bot""" # the command description can be supplied as the docstring
-    await ctx.respond(f"Hello {ctx.user}!")
+    await ctx.respond(f"Hello {ctx.author}!")
 
+@bot.command(name='global')  # creates a global slash command (might take an hour to register)
+async def global_command(ctx, num: int): # Takes one integer parameter 
+    await ctx.respond(f"This is a global command, {num}!")
 
 bot.run("TOKEN")
