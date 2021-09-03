0384a8c66d257774129a6a6f13ed2d6ab8782af0
Middledot <78228142+Middledot@users.noreply.github.com>
2021-09-03 19:05:53 -0400

    Move _BaseCommand to discord.app.commands

--- discord/ext/commands/_types.py
+++ discord/ext/commands/_types.py
@@ -40,9 +40,3 @@ CoroFunc = Callable[..., Coro[Any]]
 Check = Union[Callable[["Cog", "Context[Any]"], MaybeCoro[bool]], Callable[["Context[Any]"], MaybeCoro[bool]]]
 Hook = Union[Callable[["Cog", "Context[Any]"], Coro[Any]], Callable[["Context[Any]"], Coro[Any]]]
 Error = Union[Callable[["Cog", "Context[Any]", "CommandError"], Coro[Any]], Callable[["Context[Any]", "CommandError"], Coro[Any]]]
-
-
-# This is merely a tag type to avoid circular import issues.
-# Yes, this is a terrible solution but ultimately it is the only solution.
-class _BaseCommand:
-    __slots__ = ()
