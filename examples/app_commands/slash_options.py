916efa9e0e807008827675105c95c0611b9f435d
Bruce <86602706+BruceCodesGithub@users.noreply.github.com>
2021-09-04 09:39:02 +0530

    Added example
    Now people will understand how to allow users to select discord members and channels. Small change

--- examples/app_commands/slash_options.py
+++ examples/app_commands/slash_options.py
@@ -1,5 +1,7 @@
 import discord
 from discord.app import Option
+# if you get a import error, make sure you installed
+# pip install git+https://github.com/Pycord-Development/pycord@slash
 
 bot = discord.Bot()
 
@@ -15,3 +17,11 @@ async def hello(
     age: Option(int, "Enter your age", required=False, default=18),
 ):
     await ctx.send(f"Hello {name}")
+    
+@bot.slash_command(name="wish", description="Wish another member") # This will become a global slash command. However, global slash commands can take up to 1 hour to register.
+@commands.has_role("myRole") # make sure you have updated to the latest version of the slash branch.
+async def wish(
+    ctx,
+    member: Option(discord.Member, "The Member you want to wish")
+):
+    await ctx.send(f"{ctx.author.mention} wishes {member}!")
