ddf687b5dea58f31366d682ddb0ef8be15e628b0
unknown <codewithswastik@gmail.com>
2021-09-07 19:11:50 +0530

    Remove incorrect typehint

--- discord/app/commands.py
+++ discord/app/commands.py
@@ -461,7 +461,7 @@ class SlashCommand(ApplicationCommand):
 
 class Option:
     def __init__(
-        self, input_type: SlashCommandOptionType, /, description = None,**kwargs
+        self, input_type: Any, /, description = None,**kwargs
     ) -> None:
         self.name: Optional[str] = kwargs.pop("name", None)
         self.description = description or "No description provided"
