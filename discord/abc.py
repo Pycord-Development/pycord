7bf8ff6991fd840a970f49d51385bb5e6fb16dc5
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2021-09-09 09:18:40 -0500

    change InteractionContext to ApplicationContext

--- discord/abc.py
+++ discord/abc.py
@@ -1142,7 +1142,7 @@ class Messageable:
     - :class:`~discord.Member`
     - :class:`~discord.ext.commands.Context`
     - :class:`~discord.Thread`
-    - :class:`~discord.InteractionContext`
+    - :class:`~discord.ApplicationContext`
     """
 
     __slots__ = ()
