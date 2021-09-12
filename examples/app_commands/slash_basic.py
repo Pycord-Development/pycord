32125819e231e4c7af938f4e7cf6c495b6a4893d
Gracie <87055757+Grace-codes@users.noreply.github.com>
2021-09-12 07:33:54 +0530

    Change `ctx.send` to `ctx.respond`

--- examples/app_commands/slash_basic.py
+++ examples/app_commands/slash_basic.py
@@ -13,14 +13,14 @@ bot = discord.Bot()
 @bot.slash_command(guild_ids=[...])  # create a slash command for the supplied guilds
 async def hello(ctx):
     """Say hello to the bot"""  # the command description can be supplied as the docstring
-    await ctx.send(f"Hello {ctx.author}!")
+    await ctx.respond(f"Hello {ctx.author}!")
 
 
 @bot.slash_command(
     name="hi"
 )  # Not passing in guild_ids creates a global slash command (might take an hour to register)
 async def global_command(ctx, num: int):  # Takes one integer parameter
-    await ctx.send(f"This is a global command, {num}!")
+    await ctx.respond(f"This is a global command, {num}!")
 
 
 @bot.slash_command(guild_ids=[...])
@@ -28,8 +28,8 @@ async def joined(
     ctx, member: discord.Member = None
 ):  # Passing a default value makes the argument optional
     user = member or ctx.author
-    await ctx.send(f"{user.name} joined at {discord.utils.format_dt(user.joined_at)}")
+    await ctx.respond(f"{user.name} joined at {discord.utils.format_dt(user.joined_at)}")
 
 
-# To learn how to add descriptions, choices to options check slash_options.py
+# To learn how to add descriptions and choices to options, check slash_options.py
 bot.run("TOKEN")
