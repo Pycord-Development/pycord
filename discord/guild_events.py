5724ea7be8e39b7d1dbc27f167d76dbc8299fdfd
Middledot <78228142+Middledot@users.noreply.github.com>
2021-10-03 10:46:42 -0400

    Update license

--- discord/guild_events.py
+++ discord/guild_events.py
@@ -1,7 +1,7 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2015-present Pycord Development
+Copyright (c) 2021-present Pycord Development
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
