220f2e20a65a96d9fb06ead3c1cb16631ff8ebd5
Lala Sabathil <aiko@aitsys.dev>
2021-10-26 05:20:57 +0200

    Update commands.py

--- discord/commands/commands.py
+++ discord/commands/commands.py
@@ -58,6 +58,16 @@ __all__ = (
     "ContextMenuCommand",
     "UserCommand",
     "MessageCommand",
+    "command",
+    "application_command",
+    "slash_command",
+    "user_command",
+    "message_command",
+    "has_role",
+    "has_any_role",
+    "is_user",
+    "is_owner",
+    "permission",
 )
 
 def wrap_callback(coro):
