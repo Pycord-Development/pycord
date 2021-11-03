5c605c8cdb317e5e37835074c78efa90d08e4bc2
Lala Sabathil <lala@pycord.dev>
2021-11-03 22:37:23 +0100

    Update flags.py

--- discord/flags.py
+++ discord/flags.py
@@ -812,7 +812,7 @@ class Intents(BaseFlags):
 
         .. note::
 
-            Currently, this requires opting in explicitly via the developer portal as well.
+            Currently it requires opting in explicitly via the developer portal to receive the message content.
             Bots in over 100 guilds will need to apply to Discord for verification.
             See https://support-dev.discord.com/hc/en-us/articles/4404772028055-Message-Content-Access-Deprecation-for-Verified-Bots for more information.
         """
