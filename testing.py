cd77b4310b86706ac417c9c4f0901ec56a51d6a1
Middledot <78228142+Middledot@users.noreply.github.com>
2021-11-15 18:08:13 -0500

    Delete testing.py

--- testing.py
+++
@@ -1,7 +0,0 @@
-import pycord
-from discord.ext import commands
-
-client = commands.Bot(command_prefix="+")
-client.load_extension("jishaku")
-
-client.run("ODgxMjE1OTc3NTE2MzMxMDI4.YSpmVQ.mX6ojWeKWFr_HKfPQVY85Pkyk0c")
