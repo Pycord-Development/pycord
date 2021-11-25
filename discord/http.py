6d4f62539d08016356670c209a58d2a3c71a2f67
plun1331 <49261529+plun1331@users.noreply.github.com>
2021-11-25 11:26:18 -0800

    Update http.py

--- discord/http.py
+++ discord/http.py
@@ -550,27 +550,23 @@ class HTTPClient:
     ) -> Response[message.Message]:
         form = []
 
-        form.append({'name': 'payload_json', 'value': utils._to_json(payload)})
-        if len(files) == 1:
-            file = files[0]
+        attachments = []
+        form.append({'name': 'payload_json'})
+        for index, file in enumerate(files):
+            attachments.append({'id': index, 'filename': file.filename, 'description': file.description})
             form.append(
                 {
-                    'name': 'file',
+                    'name': f'files[{index}]',
                     'value': file.fp,
                     'filename': file.filename,
                     'content_type': 'application/octet-stream',
                 }
             )
+        if 'attachments' not in payload:
+            payload['attachments'] = attachments
         else:
-            for index, file in enumerate(files):
-                form.append(
-                    {
-                        'name': f'file{index}',
-                        'value': file.fp,
-                        'filename': file.filename,
-                        'content_type': 'application/octet-stream',
-                    }
-                )
+            payload['attachments'].extend(attachments)
+        form[0]['value'] = utils._to_json(payload)
 
         return self.request(route, form=form, files=files)
     
