f129c5101eb095f2f13bc385e0c5edb6532b8e31
RPS <vincentbusiness55@gmail.com>
2021-11-29 21:52:47 +0800

    Fix Pycord Name

--- docs/make.bat
+++ docs/make.bat
@@ -127,9 +127,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\discord.py.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\pycord.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\discord.py.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\pycord.ghc
 	goto end
 )
 
