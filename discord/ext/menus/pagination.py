450d2a637e6960eafdaadb8c1609dc0f3e0a4f7e
Krittick <ben@krittick.net>
2021-12-03 17:02:05 -0800

    almost a complete rework - needs more tweaking

--- discord/ext/menus/pagination.py
+++ discord/ext/menus/pagination.py
@@ -7,6 +7,32 @@ from discord.ext.commands import Context
 from discord.utils import MISSING
 
 
+class PaginateButton(discord.ui.Button):
+    def __init__(self, label, emoji, style, disabled, button_type, paginator):
+        super().__init__(label=label, emoji=emoji, style=style, disabled=disabled)
+        self.label = label
+        self.emoji = emoji
+        self.style = style
+        self.disabled = disabled
+        self.button_type = button_type
+        self.paginator = paginator
+
+    async def callback(self, interaction: discord.Interaction):
+        if self.button_type == "first":
+            self.paginator.current_page = 0
+        elif self.button_type == "prev":
+            self.paginator.current_page -= 1
+        elif self.button_type == "next":
+            self.paginator.current_page += 1
+        elif self.button_type == "last":
+            self.paginator.current_page = self.paginator.page_count
+        self.paginator.update_buttons()
+        page = self.paginator.pages[self.paginator.current_page]
+        await interaction.response.edit_message(
+            content=page if isinstance(page, str) else None, embed=page if isinstance(page, discord.Embed) else None, view=self.paginator
+        )
+
+
 class Paginate(discord.ui.View):
     """Creates a paginator for a message that is navigated with buttons.
     Parameters
@@ -20,63 +46,68 @@ class Paginate(discord.ui.View):
     def __init__(self, pages: Union[List[str], List[discord.Embed]], show_disabled=True, author_check=True):
         super().__init__()
         self.pages = pages
-        self.current_page = 1
-        self.page_count = len(self.pages)
+        self.current_page = 0
+        self.page_count = len(self.pages) - 1
         self.show_disabled = show_disabled
-        self.forbutton = self.children[1]
-        self.prevbutton = self.children[0]
+        self.buttons = {
+            "first": {
+                "object": PaginateButton(label="<<", style=discord.ButtonStyle.blurple, emoji=None, disabled=True, button_type="first", paginator=self),
+                "hidden": True,  # We always start by showing the first page, so there's no need to start with this button enabled
+            },
+            "prev": {
+                "object": PaginateButton(label="<", style=discord.ButtonStyle.red, emoji=None, disabled=True, button_type="prev", paginator=self),
+                "hidden": True,
+            },
+            "next": {
+                "object": PaginateButton(label=">", style=discord.ButtonStyle.green, emoji=None, disabled=True, button_type="next", paginator=self),
+                "hidden": False,
+            },
+            "last": {
+                "object": PaginateButton(label=">>", style=discord.ButtonStyle.blurple, emoji=None, disabled=True, button_type="last", paginator=self),
+                "hidden": False,
+            },
+        }
+        self.update_buttons()
+
         self.usercheck = author_check
         self.user = None
-        if not self.show_disabled:
-            self.remove_item(self.children[0])
-            if self.page_count == 1:
-                self.remove_item(self.children[1])
 
     async def interaction_check(self, interaction):
         if self.usercheck:
             return self.user == interaction.user
         return True
 
-    @discord.ui.button(label="<", style=discord.ButtonStyle.green, disabled=True)
-    async def previous(self, button: discord.ui.Button, interaction: discord.Interaction):
-
-        self.current_page -= 1
-
-        if self.current_page == 1:
-            button.disabled = True
-
-        if not self.show_disabled:
-            if len(self.children) == 1:
-                self.add_item(self.forbutton)
-                self.forbutton.disabled = False
-            if button.disabled:
-                self.remove_item(button)
-        else:
-            self.children[1].disabled = False
-
-        page = self.pages[self.current_page - 1]
-        await interaction.response.edit_message(content=page if isinstance(page, str) else None, embed=page if isinstance(page, discord.Embed) else MISSING, view=self)
-
-    @discord.ui.button(label='>', style=discord.ButtonStyle.green)
-    async def forward(self, button: discord.ui.Button, interaction: discord.Interaction):
-
-        self.current_page += 1
-
-        if self.current_page == self.page_count:
-            button.disabled = True
-
-        if not self.show_disabled:
-            if len(self.children) == 1:
-                self.add_item(self.prevbutton)
-                self.prevbutton.disabled = False
-                self.children[0], self.children[1] = self.children[1], self.children[0]
-            if button.disabled:
-                self.remove_item(button)
-        else:
-            self.children[0].disabled = False
-
-        page = self.pages[self.current_page - 1]
-        await interaction.response.edit_message(content=page if isinstance(page, str) else None, embed=page if isinstance(page, discord.Embed) else MISSING, view=self)
+    def update_buttons(self):
+        for key, button in self.buttons.items():
+            if key == "first":
+                if self.current_page <= 0:
+                    button["hidden"] = True
+                elif self.current_page >= 0 and self.page_count > 0:
+                    button["hidden"] = False
+            elif key == "prev":
+                if self.current_page <= 0:
+                    button["hidden"] = True
+                elif self.current_page >= 0 and self.page_count > 0:
+                    button["hidden"] = False
+            elif key == "next":
+                if self.current_page == self.page_count:
+                    button["hidden"] = True
+                elif self.current_page < self.page_count and self.page_count > 0:
+                    button["hidden"] = False
+            elif key == "last":
+                if self.current_page == self.page_count:
+                    button["hidden"] = True
+                if self.current_page < self.page_count and self.page_count > 0:
+                    button["hidden"] = False
+        self.clear_items()
+        for key, button in self.buttons.items():
+            if button["hidden"]:
+                button["object"].disabled = True
+                if self.show_disabled:
+                    self.add_item(button["object"])
+            else:
+                button["object"].disabled = False
+                self.add_item(button["object"])
 
     async def send(self, messageable: abc.Messageable, ephemeral: bool = False):
         """Sends a message with the paginated items.
@@ -101,43 +132,12 @@ class Paginate(discord.ui.View):
             self.user = messageable.author
 
         if isinstance(messageable, ApplicationContext):
-            message = await messageable.respond(content=page if isinstance(page, str) else None, embed=page if isinstance(page, discord.Embed) else MISSING, view=self, ephemeral=ephemeral)
+            message = await messageable.respond(
+                content=page if isinstance(page, str) else None, embed=page if isinstance(page, discord.Embed) else MISSING, view=self, ephemeral=ephemeral
+            )
         else:
-            message = await messageable.send(content=page if isinstance(page, str) else None, embed=page if isinstance(page, discord.Embed) else None, view=self)
+            message = await messageable.send(
+                content=page if isinstance(page, str) else None, embed=page if isinstance(page, discord.Embed) else None, view=self
+            )
         return message
 
-    def forward_button(self, label: str, color: str = "green"):
-        """Customize your forward button
-        Parameters
-        ------------
-        label: :class:`str`
-            The text you want on your button
-        color: :class:`str`
-            The color of the button.
-        Raises
-        --------
-        AttributeError
-            The color provided isn't supported
-            """
-
-        self.forbutton.label = label
-        color = getattr(discord.ButtonStyle, color.lower())
-        self.forbutton.style = color
-
-    def back_button(self, label: str, color: str = "green"):
-        """Customize your back button
-        Parameters
-        ------------
-        label: :class:`str`
-            The text you want on your button
-        color: :class:`str`
-            The color of the button.
-        Raises
-        --------
-        AttributeError
-            The color provided isn't supported
-            """
-
-        self.prevbutton.label = label
-        color = getattr(discord.ButtonStyle, color.lower())
-        self.prevbutton.style = color
