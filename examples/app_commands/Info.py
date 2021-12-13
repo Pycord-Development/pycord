eeb9a488f04430bd66f2ef2987b1e1ad0dd5189f
Lala Sabathil <aiko@aitsys.dev>
2021-12-13 06:35:53 +0100

    Apply suggestions from code review
    Co-authored-by: Vincent <vincentbusiness55@gmail.com>
    Co-authored-by: krittick <ben@krittick.net>

--- examples/app_commands/Info.py
+++ examples/app_commands/Info.py
@@ -8,18 +8,17 @@ intents = discord.Intents(
             messages=True,
             )
 
-bot = commands.Bot(command_prefix=".", description="An example to showcase how to extract info about users",intents=intents)
+bot = commands.Bot(command_prefix="/", description="An example to showcase how to extract info about users",intents=intents)
 
 @bot.slash_command(name="userinfo", description="gets the info of a user")
 async def info(ctx, user: discord.Member = None):
     user = user or ctx.author #if no user is provided it'll use the the author of the message
     e = discord.Embed()
     e.set_author(name=user.name)
-    e.add_field(name='ID', value=user.id, inline=False) # user ID
+    e.add_field(name="ID", value=user.id, inline=False) # user ID
     e.add_field(name='Joined',
                 value=f"<t:{round(user.joined_at.timestamp())}:F>", inline=False) # When the user joined the server
-    e.add_field(name='Created',
-                value=f"<t:{round(user.created_at.timestamp())}:F>", inline=False) # When the user's account was created
+    e.add_field(name="Created", value=discord.utils.format_dt(round(user.created_at.timestamp()), "F"), inline=False) # When the user's account was created
     colour = user.colour
     if colour.value: # if user has a role with a color
            e.colour = colour
@@ -29,4 +28,4 @@ async def info(ctx, user: discord.Member = None):
 
     await ctx.respond(embed=e) # sends the embed
 
-bot.run("urtoken")
+bot.run("your token")
