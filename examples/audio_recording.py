dc636208db182e61f8c600cd752a52fb458fed77
Vincent <vincentbusiness55@gmail.com>
2021-12-19 14:10:45 +0800

    Redo Voice Example To Use Bot Instead of Client
    Co-Authored-By: Swas.py <61446939+CodeWithSwastik@users.noreply.github.com>

--- examples/audio_recording.py
+++ examples/audio_recording.py
@@ -1,153 +1,55 @@
-import discord
 import os
+import discord
+from discord.commands import Option
 
-def vc_required(func):
-    async def get_vc(self, msg):
-        vc = await self.get_vc(msg)
-        if not vc:
-            return
-        await func(self, msg, vc)
-
-    return get_vc
+bot = discord.Bot(debug_guilds=[...])
+bot.connections = {}
 
 
-def args_to_filters(args):
-    filters = {}
-    if "--time" in args:
-        index = args.index("--time")
-        try:
-            seconds = args[index + 1]
-        except IndexError:
-            return "You must provide an amount of seconds for the time."
-        try:
-            seconds = int(seconds)
-        except ValueError:
-            return "You must provide an integer value."
-        filters.update({"time": seconds})
-    if "--users" in args:
-        users = []
-        index = args.index("--users") + 1
-        while True:
-            try:
-                users.append(int(args[index]))
-            except IndexError:
-                break
-            except ValueError:
-                break
-            index += 1
-        if not users:
-            return "You must provide at least one user, or multiple users separated by spaces."
-        filters.update({"users": users})
-    return filters
+@bot.command()
+async def start(ctx, encoding: Option(str, choices=["mp3", "wav", "pcm"])):
+    """
+    Record your voice!
+    """
+
+    voice = ctx.author.voice
+
+    if not voice:
+        return await ctx.respond("You're not in a vc right now")
+
+    vc = await voice.channel.connect()
+    bot.connections.update({ctx.guild.id: vc})
+
+    vc.start_recording(
+        discord.Sink(encoding=encoding),
+        finished_callback,
+        ctx.channel,
+    )
+    
+    await ctx.respond("The recording has started!")
 
 
-def get_encoding(args):
-    if "--output" in args:
-        index = args.index("--output")
-        try:
-            encoding = args[index + 1].lower()
-            if encoding not in discord.Sink.valid_encodings:
-                return
-            return encoding
-        except IndexError:
-            return
-    else:
-        """
-        Currently Supported:
+async def finished_callback(sink, channel, *args):
 
-        mp3
-        wave(.wav)
-        pcm
-        """
-        return "mp3"
+    recorded_users = [
+        f" <@{user_id}> ({os.path.split(audio.file)[1]}) "
+        for user_id, audio in sink.audio_data.items()
+    ]
+    await sink.vc.disconnect()
+    await channel.send(f"Finished! Recorded audio for {', '.join(recorded_users)}.")
 
-
-class Client(discord.Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.connections = {voice.guild.id: voice for voice in self.voice_clients}
-        self.playlists = {}
-
-        self.commands = {
-            "!start": self.start_recording,
-            "!stop": self.stop_recording,
-            "!pause": self.toggle_pause,
-        }
-
-    async def get_vc(self, message):
-        vc = message.author.voice
-        if not vc:
-            await message.channel.send("You're not in a vc right now")
-            return
-        connection = self.connections.get(message.guild.id)
-        if connection:
-            if connection.channel.id == message.author.voice.channel.id:
-                return connection
-
-            await connection.move_to(vc.channel)
-            return connection
-        else:
-            vc = await vc.channel.connect()
-            self.connections.update({message.guild.id: vc})
-            return vc
-
-    async def on_message(self, msg):
-        if not msg.content:
-            return
-        cmd = msg.content.split()[0]
-        if cmd in self.commands:
-            await self.commands[cmd](msg)
-
-    @vc_required
-    async def start_recording(self, msg, vc):
-        args = msg.content.split()[1:]
-        filters = args_to_filters(args)
-        if type(filters) == str:
-            return await msg.channel.send(filters)
-        encoding = get_encoding(args)
-        if encoding is None:
-            return await msg.channel.send("You must provide a valid output encoding.")
-
-        vc.start_recording(
-            discord.Sink(encoding=encoding, filters=filters),
-            self.finished_callback,
-            msg.channel,
-        )
-
-        await msg.channel.send("The recording has started!")
-
-    @vc_required
-    async def toggle_pause(self, msg, vc):
-        vc.toggle_pause()
-        await msg.channel.send(
-            f"The recording has been {'paused' if vc.paused else 'unpaused'}"
-        )
-
-    @vc_required
-    async def stop_recording(self, msg, vc):
+@bot.command()
+async def stop(ctx):
+    """
+    Stop recording.
+    """
+    if ctx.guild.id in bot.connections:
+        vc = bot.connections[ctx.guild.id]
         vc.stop_recording()
-
-    async def finished_callback(self, sink, channel, *args):
-        # Note: sink.audio_data = {user_id: AudioData}
-        recorded_users = [
-            f" <@{str(user_id)}> ({os.path.split(audio.file)[1]}) "
-            for user_id, audio in sink.audio_data.items()
-        ]
-        await channel.send(f"Finished! Recorded audio for {', '.join(recorded_users)}.")
-
-    async def on_voice_state_update(self, member, before, after):
-        if member.id != self.user.id:
-            return
-        # Filter out updates other than when we leave a channel we're connected to
-        if member.guild.id not in self.connections and (
-            not before.channel and after.channel
-        ):
-            return
-
-        print("Disconnected")
-        del self.connections[member.guild.id]
+        del bot.connections[ctx.guild.id]
+        await ctx.delete()
+    else:
+        await ctx.respond("Not recording in this guild.")
 
 
-intents = discord.Intents.default()
-client = Client(intents=intents)
-client.run("TOKEN")
+bot.run("TOKEN")
