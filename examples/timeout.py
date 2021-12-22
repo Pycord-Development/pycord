e11317a792cc7c2fa3ca40bf637076700e06f9cc
Dorukyum <doruk.ak@hotmail.com>
2021-12-22 18:03:00 +0300

    Add timeout example

---
+++ examples/timeout.py
@@ -0,0 +1,18 @@
+import datetime
+
+import discord
+
+bot = discord.Bot()
+
+
+@bot.command()
+async def timeout(ctx, member: discord.Member, minutes: int):
+    """Set timeout on a member"""
+
+    now = datetime.datetime.now()  # get current time
+    until = now + datetime.timedelta(minutes=minutes)  # add the duration
+    await member.timeout(until=until)
+    await ctx.respond(f"Member timed out for {minutes} minutes.")
+
+
+bot.run("TOKEN")
