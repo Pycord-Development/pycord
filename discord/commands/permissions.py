c82bb90c2932e5b45163e82c27802f691d704938
EmreTech <50607143+EmreTech@users.noreply.github.com>
2021-12-24 22:37:43 -0800

    Whoops forgot the cog include

--- discord/commands/permissions.py
+++ discord/commands/permissions.py
@@ -23,7 +23,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-from ..cog import Cog
 from typing import Union, Dict, Callable
 
 __all__ = (
