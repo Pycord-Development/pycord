ce20d741e23ae70ade169d1d40df3b9c4db39683
Dorukyum <53639936+Dorukyum@users.noreply.github.com>
2021-12-26 17:45:44 +0300

    Use `Member.timeout_for`

--- examples/timeout.py
+++ examples/timeout.py
@@ -7,12 +7,18 @@ bot = discord.Bot()
 
 @bot.command()
 async def timeout(ctx, member: discord.Member, minutes: int):
-    """Set timeout on a member"""
+    """Apply a timeout to a member"""
 
-    now = discord.utils.utcnow()  # get current time
-    until = now + datetime.timedelta(minutes=minutes)  # add the duration
-    await member.timeout(until=until)
-    await ctx.respond(f"Member timed out for {minutes} minutes.")
+    duration = datetime.timedelta(minutes=minutes)
+    await member.timeout_for(duration)
+    await ctx.reply(f"Member timed out for {minutes} minutes.")
+
+    """
+    The method used above is a shortcut for
+
+    until = discord.utils.utcnow() + datetime.timedelta(minutes=minutes)
+    await member.timeout(until)
+    """
 
 
 bot.run("TOKEN")
