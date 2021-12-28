26feb1c7eb8cd505ae68fdcc01b63c96edcbfb13
Krittick <ben@krittick.net>
2021-12-27 23:03:47 -0800

    apply formatting changes

--- discord/commands/context.py
+++ discord/commands/context.py
@@ -45,10 +45,8 @@ from ..message import Message
 from ..user import User
 from ..utils import cached_property
 
-__all__ = (
-    "ApplicationContext",
-    "AutocompleteContext"
-)
+__all__ = ("ApplicationContext", "AutocompleteContext")
+
 
 class ApplicationContext(discord.abc.Messageable):
     """Represents a Discord application command interaction context.
@@ -127,7 +125,7 @@ class ApplicationContext(discord.abc.Messageable):
     def voice_client(self):
         if self.guild is None:
             return None
-        
+
         return self.guild.voice_client
 
     @cached_property
@@ -139,7 +137,9 @@ class ApplicationContext(discord.abc.Messageable):
         if not self.response.is_done():
             return self.interaction.response.send_message
         else:
-            raise RuntimeError(f"Interaction was already issued a response. Try using {type(self).__name__}.send_followup() instead.")
+            raise RuntimeError(
+                f"Interaction was already issued a response. Try using {type(self).__name__}.send_followup() instead."
+            )
 
     @property
     def send_response(self) -> Callable[..., Union[Interaction, Webhook]]:
@@ -155,7 +155,9 @@ class ApplicationContext(discord.abc.Messageable):
         if self.response.is_done():
             return self.followup.send
         else:
-            raise RuntimeError(f"Interaction was not yet issued a response. Try using {type(self).__name__}.respond() first.")
+            raise RuntimeError(
+                f"Interaction was not yet issued a response. Try using {type(self).__name__}.respond() first."
+            )
 
     @property
     def defer(self):
@@ -182,7 +184,7 @@ class ApplicationContext(discord.abc.Messageable):
         """Optional[:class:`.Cog`]: Returns the cog associated with this context's command. ``None`` if it does not exist."""
         if self.command is None:
             return None
-       
+
         return self.command.cog
 
 
@@ -196,7 +198,7 @@ class AutocompleteContext:
     Attributes
     -----------
     bot: :class:`.Bot`
-        The bot that the command belongs to.    
+        The bot that the command belongs to.
     interaction: :class:`.Interaction`
         The interaction object that invoked the autocomplete.
     command: :class:`.ApplicationCommand`
@@ -210,7 +212,7 @@ class AutocompleteContext:
     """
 
     __slots__ = ("bot", "interaction", "command", "focused", "value", "options")
-    
+
     def __init__(self, bot: Bot, interaction: Interaction) -> None:
         self.bot = bot
         self.interaction = interaction
@@ -225,5 +227,5 @@ class AutocompleteContext:
         """Optional[:class:`.Cog`]: Returns the cog associated with this context's command. ``None`` if it does not exist."""
         if self.command is None:
             return None
-       
+
         return self.command.cog
