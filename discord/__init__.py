5c41bb06dae358f0f3b82421cda405fd15023349
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2022-01-28 18:56:48 -0600

    Prep for beta 1

--- discord/__init__.py
+++ discord/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'discord'
 __author__ = 'Pycord Development'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-2021 Rapptz & Copyright 2021-present Pycord Development'
-__version__ = '2.0.0b'
+__version__ = '2.0.0b1'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
