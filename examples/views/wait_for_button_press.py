312b6d1edce059f419daf9a8dd0613b788c3324d
its_florent <70605487+itsflorent@users.noreply.github.com>
2022-01-29 21:42:25 +0100

    Update wait_for_button_press.py

--- examples/views/wait_for_button_press.py
+++ examples/views/wait_for_button_press.py
@@ -1,15 +1,15 @@
+import asyncio
+
 import discord
 from discord.ext import commands
 from discord.ui import Button, View
-import asyncio
+
 
 bot = commands.Bot(command_prefix="!")
 
 # Create a slash command for the supplied guilds.
-@bot.slash_command(name="button", guild_ids=[...], description="Wait for button press example from py-cord") 
+@bot.slash_command(name="button", guild_ids=[...], description="Wait for button press example from pycord") 
 async def button(ctx):
-    await ctx.defer()
-    
     ###############
     # Create button and add it to View()
     button = Button(style=discord.ButtonStyle.green, label="Click me!")
@@ -22,7 +22,8 @@ async def button(ctx):
     embed = discord.Embed(description=f'Wait for button press example')
     embed.set_author(name=f'Pycord-Development')
     
-    msg = await ctx.send_followup(embed=embed, view=view)
+    await ctx.respond(embed=embed, view=view)
+    msg = await ctx.interaction.original_message()
     ###############
     
     ###############
@@ -36,7 +37,7 @@ async def button(ctx):
         interaction = await bot.wait_for('interaction', check=check, timeout=30)
         # Wait for the user to press the button.
 
-        if f"{button.custom_id}" in str(interaction.data):
+        if button.custom_id == interaction.data["custom_id"]:
             # Checks whether the custom_id of the button we specified above is the same as the one the user pressed.
             await msg.reply("Button pressed!")
     
