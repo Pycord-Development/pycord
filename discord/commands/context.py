b1024cd65bba8695e13289a0cf929cc408d52e0b
Krittick <ben@krittick.net>
2022-02-05 03:12:14 -0800

    better pythonicness

--- discord/commands/context.py
+++ discord/commands/context.py
@@ -174,22 +174,20 @@ class ApplicationContext(discord.abc.Messageable):
 
     @property
     def selected_options(self) -> Optional[List[Dict]]:
-        """Returns a dictionary containing the options and values that were selected by the user, if applicable."""
+        """Returns a dictionary containing the options and values that were selected by the user when the command was processed, if applicable."""
         if "options" in self.interaction.data:
             return self.interaction.data["options"]
         return None
 
     @property
-    def unselected_options(self) -> Optional[List[Dict]]:
-        """Returns a dictionary containing the options that were not selected by the user, if applicable."""
+    def unselected_options(self) -> Optional[List[Option]]:
+        """Returns a list of Option objects (if any) that were not selected by the user when the command was processed."""
         if self.command.options is not None:  # type: ignore
-            selected = [opt["name"] for opt in self.selected_options]
             return [
                 option
                 for option in self.command.options  # type: ignore
-                if option.to_dict()["name"] not in selected
+                if option.to_dict()["name"] not in [opt["name"] for opt in self.selected_options]
             ]
-
         return None
 
     @property
