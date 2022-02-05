ec075deb8cf39b0b35ac62b35d6668b5186dc312
Dorukyum <doruk.ak@hotmail.com>
2022-02-05 23:26:55 +0300

    Remove unnecessary type conversion

--- discord/bot.py
+++ discord/bot.py
@@ -576,7 +576,7 @@ class ApplicationCommandMixin:
             guild_permissions: List = []
 
             for i in commands:
-                cmd = find(lambda cmd: cmd.name == i["name"] and str(cmd.type) == str(i["type"]) and cmd.guild_ids is not None
+                cmd = find(lambda cmd: cmd.name == i["name"] and cmd.type == i["type"] and cmd.guild_ids is not None
                                        and int(i["guild_id"]) in cmd.guild_ids, self.pending_application_commands)
                 if not cmd:
                     # command has not been added yet
