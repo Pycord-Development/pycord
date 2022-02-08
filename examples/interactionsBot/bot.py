7ad90b18bf11b5625d766e411bf87a6bd9112b7c
Custom <53391982+PythonIntermediateCoder@users.noreply.github.com>
2022-02-08 16:54:38 +0200

    add comment about type: ignore
    Mypy counts "if" statement inside a list comprehension as an "if/else" statement.

--- examples/interactionsBot/bot.py
+++ examples/interactionsBot/bot.py
@@ -31,6 +31,7 @@ class BotClass(discord.Bot):
 bot = BotClass()
 
 py_files = list(file.group(1) if PY_FILE_REGEX.match(file) for file in os.listdir("./cogs")) # type: ignore
+# mypy counts "if" statement inside a list comprehension as an "if/else" statement.
 for file in py_files:
     bot.load_extension(f"cogs.{file}")
 
