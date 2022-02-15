19226949b8230993644ae19d53f59140eb5f4c48
plun1331 <49261529+plun1331@users.noreply.github.com>
2022-02-15 07:10:15 -0800

    Merge pull request #1014 from plun1331/patch-14
    Add Message Content Intent

--- discord/flags.py
+++ discord/flags.py
@@ -525,11 +525,12 @@ class Intents(BaseFlags):
     @classmethod
     def default(cls: Type[Intents]) -> Intents:
         """A factory method that creates a :class:`Intents` with everything enabled
-        except :attr:`presences` and :attr:`members`.
+        except :attr:`presences`, :attr:`members`, and :attr:`message_content`.
         """
         self = cls.all()
         self.presences = False
         self.members = False
+        self.message_content = False
         return self
 
     @flag_value
@@ -749,9 +750,7 @@ class Intents(BaseFlags):
 
         .. note::
 
-            As of April 2022 requires opting in explicitly via the developer portal to receive the actual content of the guild messages.
-            Bots in over 100 guilds will need to apply to Discord for verification.
-            See https://support-dev.discord.com/hc/en-us/articles/4404772028055-Message-Content-Access-Deprecation-for-Verified-Bots for more information.
+            :attr:`message_content` is required to receive the actual content of guild messages.
         """
         return (1 << 9) | (1 << 12)
 
@@ -780,7 +779,7 @@ class Intents(BaseFlags):
         - :func:`on_reaction_remove` (only for guilds)
         - :func:`on_reaction_clear` (only for guilds)
 
-        Without the :attr:`ApplicationFlags.gateway_message_content` intent enabled, the following fields are either an empty string or empty array:
+        Without the :attr:`message_content` intent enabled, the following fields are either an empty string or empty array:
 
         - :attr:`Message.content`
         - :attr:`Message.embeds`
@@ -788,12 +787,6 @@ class Intents(BaseFlags):
         - :attr:`Message.components`
 
         For more information go to the :ref:`message content intent documentation <need_message_content_intent>`.
-
-        .. note::
-
-            As of April 2022 requires opting in explicitly via the developer portal to receive the actual content of the messages.
-            Bots in over 100 guilds will need to apply to Discord for verification.
-            See https://support-dev.discord.com/hc/en-us/articles/4404772028055-Message-Content-Access-Deprecation-for-Verified-Bots for more information.
         """
         return 1 << 9
 
@@ -928,6 +921,31 @@ class Intents(BaseFlags):
         This does not correspond to any attributes or classes in the library in terms of cache.
         """
         return 1 << 14
+
+    @flag_value
+    def message_content(self):
+        """:class:`bool`: Whether the bot will receive message content in guild messages.
+
+        This corresponds to the following attributes:
+
+        - :attr:`Message.content`
+        - :attr:`Message.embeds`
+        - :attr:`Message.attachments`
+        - :attr:`Message.components`
+
+        These attributes will still be available for messages received from interactions,
+        the bot's own messages, messages the bot was mentioned in, and DMs.
+
+        .. versionadded:: 2.0
+
+        .. note::
+
+            As of April 2022 requires opting in explicitly via the developer portal to receive the actual content of the guild messages.
+            Bots in over 100 guilds will need to apply to Discord for verification.
+            See https://support-dev.discord.com/hc/en-us/articles/4404772028055 for more information.
+
+        """
+        return 1 << 15
 
     @flag_value
     def scheduled_events(self):
