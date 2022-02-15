a6785f15d5b5bb0db14e7570df9b28f533065065
krittick <ben@krittick.net>
2022-02-15 07:53:06 -0800

    Update discord/ui/button.py
    Co-authored-by: Dorukyum <53639936+Dorukyum@users.noreply.github.com>

--- discord/ui/button.py
+++ discord/ui/button.py
@@ -99,7 +99,7 @@ class Button(Item[V]):
         if custom_id is not None and url is not None:
             raise TypeError("cannot mix both url and custom_id with Button")
 
-        if not isinstance(custom_id, Union[str, None]):
+        if not (isinstance(custom_id, str) or custom_id is None):
             raise TypeError(f"expected custom_id to be str, not {type(custom_id).__name__}")
 
         self._provided_custom_id = custom_id is not None
