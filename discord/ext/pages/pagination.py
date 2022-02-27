29ddfbc690b847eff303d0017062a361d286110b
Krittick <ben@krittick.net>
2022-02-26 21:45:56 -0800

    fix for #1082

--- discord/ext/pages/pagination.py
+++ discord/ext/pages/pagination.py
@@ -374,7 +374,9 @@ class Paginator(discord.ui.View):
         """
         page = self.get_page_content(page)
         for item in self.children:
-            if item not in self.custom_view.children or include_custom:
+            if self.custom_view and (
+                item not in self.custom_view.children or include_custom
+            ):
                 item.disabled = True
         if page:
             await self.message.edit(
