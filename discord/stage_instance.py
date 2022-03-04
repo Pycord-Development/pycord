bfdbfbaf8e3afb81c9b6b67932121f119c633dea
Middledot <middledot.productions@gmail.com>
2022-03-03 21:03:56 -0500

    Fix scheduled_event attribute problems

--- discord/stage_instance.py
+++ discord/stage_instance.py
@@ -86,6 +86,7 @@ class StageInstance(Hashable):
         "topic",
         "privacy_level",
         "discoverable_disabled",
+        "scheduled_event",
         "_cs_channel",
     )
 
@@ -100,7 +101,10 @@ class StageInstance(Hashable):
         self.topic: str = data["topic"]
         self.privacy_level: StagePrivacyLevel = try_enum(StagePrivacyLevel, data["privacy_level"])
         self.discoverable_disabled: bool = data.get("discoverable_disabled", False)
-        self.scheduled_event = self.guild.get_scheduled_event(int(data.get("guild_scheduled_event_id")))
+        event_id = data.get("guild_scheduled_event")
+        if event_id is not None:
+            event_id = int(event_id)
+        self.scheduled_event = self.guild.get_scheduled_event(event_id)
 
     def __repr__(self) -> str:
         return f"<StageInstance id={self.id} guild={self.guild!r} channel_id={self.channel_id} topic={self.topic!r}>"
