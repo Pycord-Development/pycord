Pycord-Development/pycord

commit 942b5cca4dbb4fe3f8c80a2502e1059672cc111f

Fix keyword arguments failing (#1127)

discord/commands/core.py
@@ -886,7 +886,9 @@ class SlashCommandGroup(ApplicationCommand):
         description: str,
         guild_ids: Optional[List[int]] = None,
         parent: Optional[SlashCommandGroup] = None,
-        **kwargs,
+        *,
+        default_permissions: Optional[bool] = True,
+        permissions: Optional[List[CommandPermission]] = [],
     ) -> None:
         validate_chat_input_name(name)
         validate_chat_input_description(description)
@@ -904,8 +906,8 @@ class SlashCommandGroup(ApplicationCommand):
         self.id = None
 
         # Permissions
-        self.default_permission = kwargs.get("default_permission", True)
-        self.permissions: List[CommandPermission] = kwargs.get("permissions", [])
+        self.default_permission = default_permissions
+        self.permissions: List[CommandPermission] = permissions
         if self.permissions and self.default_permission:
             self.default_permission = False
 

commit 9c64c29b5e4804dee2dd9188ef8f563f01e9471e

add missing **kwargs param to support non-explicit parameters in `SlashCommandGroup` (fixes issue introduced by #1127)

discord/commands/core.py
@@ -889,6 +889,7 @@ class SlashCommandGroup(ApplicationCommand):
         *,
         default_permissions: Optional[bool] = True,
         permissions: Optional[List[CommandPermission]] = [],
+        **kwargs,
     ) -> None:
         validate_chat_input_name(name)
         validate_chat_input_description(description)