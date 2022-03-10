e34d4a8c38d1833e137ae2daeb19031caeb878cb
plun1331 <49261529+plun1331@users.noreply.github.com>
2022-03-10 10:40:03 -0800

    Update bot.py

--- discord/bot.py
+++ discord/bot.py
@@ -8,7 +8,7 @@ Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation
 the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
+and/or sell copies of the Software, and to permit persons to whom thed
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in
@@ -817,7 +817,7 @@ class ApplicationCommandMixin:
         return decorator
 
     def command(self, **kwargs):
-        """There is an alias for :meth:`application_command`.
+        """An alias for :meth:`application_command`.
 
         .. note::
 
