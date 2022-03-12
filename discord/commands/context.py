b5f6ad6403d2cea1ce6e2625f2a887e7e2725e58
Dorukyum <53639936+Dorukyum@users.noreply.github.com>
2022-03-12 21:08:42 +0300

    Fix typehints for `send_modal` methods (#1157)
    * Fix typehints for `send_modal` methods
    
    * Keep InteractionResponse.send_modal typehint

--- discord/commands/context.py
+++ discord/commands/context.py
@@ -216,7 +216,7 @@ class ApplicationContext(discord.abc.Messageable):
         return None
 
     @property
-    def send_modal(self) -> Interaction:
+    def send_modal(self) -> Callable[..., Awaitable[Interaction]]:
         """Sends a modal dialog to the user who invoked the interaction."""
         return self.interaction.response.send_modal
 
