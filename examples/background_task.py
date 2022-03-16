e450f72d97333d425fc31a99eb38dfaf7e5e76a2
Timo <tibue69@gmail.com>
2022-03-16 18:36:56 +0100

    Simplify datetime task example

--- examples/background_task.py
+++ examples/background_task.py
@@ -5,9 +5,6 @@ from discord.ext import tasks
 
 
 class MyClient(discord.Client):
-    # You can specify a time objects for your task
-    run_time = time(3, 0, tzinfo=timezone.utc)
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -28,7 +25,9 @@ class MyClient(discord.Client):
         self.counter += 1
         await channel.send(self.counter)
 
-    @tasks.loop(time=run_time)  # Task runs every day at 3 AM
+    @tasks.loop(
+        time=time(3, 0, tzinfo=timezone.utc)  # Task that runs every day at 3 AM UTC
+    )
     async def time_task(self):
         channel = self.get_channel(1234567)  # Your Channel ID goes here
         await channel.send("It's 3 AM!")
