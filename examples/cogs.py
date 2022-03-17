4966756699dd04f0029804901d1582f64d8cfb6a
27Saumya <64534496+27Saumya@users.noreply.github.com>
2022-03-17 13:47:48 +0530

    add `cogs` in `examples`

---
+++ examples/cogs.py
@@ -0,0 +1,35 @@
+# This will be your cog file inside the "cogs" folder
+
+# cogs/whatever.py
+
+from discord.ext commands
+
+class MyCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    # basic ping command
+    @commands.command #since we can't use bot (Will return error: "bot is not defined" we use "commands.command"
+    async def ping(self, ctx: commands.Context):
+        await ctx.send("Pong!")
+
+    # command group "test"
+    @commands.group()
+    async def test(self, ctx: commands.Context):
+        pass
+
+    # ping command of group "test"
+    @test.command()
+    async def ping(self, ctx: commands.Context):
+        await ctx.send("Pong!")
+
+# Main file Eg: "bot.py:
+
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!")
+
+bot.load_extension("whatever") # load the whatever.py
+
+bot.run("token")
+    
