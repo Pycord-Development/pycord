a9d6bbf363b350b1d570c237f0d5d3ef15786bba
That GenZ Gamer <78960084+ThatGenZGamer48@users.noreply.github.com>
2022-03-26 23:01:04 +0530

    Add requested changes.

--- examples/bridge_bot.py
+++ examples/bridge_bot.py
@@ -8,11 +8,11 @@ intents.message_content = True
 bot = bridge.Bot(command_prefix="!", intents=intents)
 
 @bot.bridge_command()
-async def sayhello(ctx):
+async def hello(ctx):
     await ctx.respond("Hello!")
 
-@bot.bridge_command()
-async def getmemberid(ctx, *, member: discord.Member):
+@bot.bridge_command(name="getmemberid")
+async def get_member_id(ctx, *, member: discord.Member):
     await ctx.respond(f"The member's ID is {member.id}")
 
-bot.run("Your token here")
+bot.run("TOKEN")
