2fdd162e7ada6a22bcbde72a76764ac0bc1dc68c
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-04-12 12:29:36 +0530

    Update abc.py

--- discord/abc.py
+++ discord/abc.py
@@ -519,7 +519,7 @@ class GuildChannel:
 
         .. versionadded:: 2.0
         """
-        return f'https://discord.com/channels/{self.guild.id}/{self.id}'
+        return f"https://discord.com/channels/{self.guild.id}/{self.id}"
 
     @property
     def created_at(self) -> datetime:
