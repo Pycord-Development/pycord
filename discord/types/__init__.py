d371715e8a26cdd9551567df67ca6ded828134a2
kseiru <kseiru@knteam.ru>
2022-05-10 23:50:54 +0300

    remove discord
    i just russian translator 🥵🥵🥵🥵

--- discord/types/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-discord.types
-~~~~~~~~~~~~~~
-
-Typings for the Discord API
-
-:copyright: (c) 2015-2021 Rapptz & (c) 2021-present Pycord Development
-:license: MIT, see LICENSE for more details.
-"""
