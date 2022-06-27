9f035dcec69f0b06903c59d51744b3fc9d9ff079
Enoki <enokiun@gmail.com>
2022-06-27 15:54:57 +0300

    chore: improve `ApplicationContext.respond` doc.

--- discord/commands/context.py
+++ discord/commands/context.py
@@ -126,6 +126,7 @@ class ApplicationContext(discord.abc.Messageable):
             The arguments to use.
         \*\*kwargs
             The keyword arguments to use.
+
         Raises
         -------
         TypeError
@@ -221,7 +222,15 @@ class ApplicationContext(discord.abc.Messageable):
         return self.interaction.response.send_modal
 
     async def respond(self, *args, **kwargs) -> Union[Interaction, WebhookMessage]:
-        """Sends either a response or a followup response depending if the interaction has been responded to yet or not."""
+        """|coro|
+
+        Sends either a response or a followup response depending if the interaction has been responded to yet or not.
+
+        Returns
+        -------
+        Union[:class:`discord.Interaction`, :class:`discord.WebhookMessage`]: The message that was sent, it's type depends on whether
+            the message was an interaction reply or a followup message.
+        """
         try:
             if not self.interaction.response.is_done():
                 return await self.interaction.response.send_message(*args, **kwargs)  # self.response
