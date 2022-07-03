bafa80b4fba756249cdb54338b2b5351121aef62
gigabit- <49173264+TheGiga@users.noreply.github.com>
2022-07-03 21:13:28 +0300

    possible fix

--- examples/create_private_emoji.py
+++ examples/create_private_emoji.py
@@ -11,13 +11,13 @@ allowed_content_types = ['image/jpeg', 'image/png']  # Setting up allowed attach
 @bot.command(guild_ids=[...])
 async def add_private_emoji(
         ctx, name: discord.Option(str),
-        attachment: discord.Option(discord.Attachment, name='image'),
+        image: discord.Option(discord.Attachment),
         role: discord.Option(discord.Role)
 ):
-    if attachment.content_type not in allowed_content_types:
+    if image.content_type not in allowed_content_types:
         return await ctx.respond(ephemeral=True, content='Invalid attachment type!')
 
-    image_file = await attachment.read()  # Reading attachment's content to get bytes
+    image_file = await image.read()  # Reading attachment's content to get bytes
 
     await ctx.guild.create_custom_emoji(name=name, image=image_file, roles=[role])  # Image argument only takes bytes!
     await ctx.respond(content=f'Private emoji is successfully created!')
