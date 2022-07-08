192104d630d8386053d0b01ecb76a2b830d378f2
Lala Sabathil <lala@pycord.dev>
2022-07-08 21:00:51 +0200

    Bumb to v2.0.0

--- discord/__init__.py
+++ discord/__init__.py
@@ -13,7 +13,7 @@ __title__ = "pycord"
 __author__ = "Pycord Development"
 __license__ = "MIT"
 __copyright__ = "Copyright 2015-2021 Rapptz & Copyright 2021-present Pycord Development"
-__version__ = "2.0.0rc1"
+__version__ = "2.0.0"
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
