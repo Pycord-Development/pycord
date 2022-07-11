549c33f46a7cf405411366505e6c55b1de7a4415
Lala Sabathil <aiko@aitsys.dev>
2022-07-11 03:25:01 +0200

    Update discord/flags.py

--- discord/flags.py
+++ discord/flags.py
@@ -1258,7 +1258,7 @@ class ApplicationFlags(BaseFlags):
             Note that aliases are not shown.
 
         .. describe:: x + y
-         Adds two flags together.
+        Adds two flags together.
 
         .. describe:: x - y
         Subtracts two flags from each other.
