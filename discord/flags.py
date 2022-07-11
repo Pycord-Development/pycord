c686624b8d0c883310700b0d4d1be2a0512b3971
Lala Sabathil <aiko@aitsys.dev>
2022-07-11 03:25:17 +0200

    Update discord/flags.py

--- discord/flags.py
+++ discord/flags.py
@@ -1121,7 +1121,7 @@ class MemberCacheFlags(BaseFlags):
                to be, for example, constructed as a dict or a list of pairs.
 
         .. describe:: x + y
-         Adds two flags together.
+        Adds two flags together.
 
         .. describe:: x - y
         Subtracts two flags from each other.
