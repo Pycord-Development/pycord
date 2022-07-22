e4be6dc0c405d602dbe7f57836cbd979e477e7fa
Lala Sabathil <aiko@aitsys.dev>
2022-07-22 20:59:41 +0200

    Update discord/stage_instance.py
    Co-authored-by: Middledot <78228142+Middledot@users.noreply.github.com>

--- discord/stage_instance.py
+++ discord/stage_instance.py
@@ -102,10 +102,9 @@ class StageInstance(Hashable):
         self.privacy_level: StagePrivacyLevel = try_enum(StagePrivacyLevel, data["privacy_level"])
         self.discoverable_disabled: bool = data.get("discoverable_disabled", False)
 
-        if (event_id := data.get("guild_scheduled_event_id", None)) is not None:
+        self.scheduled_event = None
+        if event_id := data.get("guild_scheduled_event_id", None):
             self.scheduled_event = self.guild.get_scheduled_event(int(event_id))
-        else:
-            self.scheduled_event = None
 
     def __repr__(self) -> str:
         return f"<StageInstance id={self.id} guild={self.guild!r} channel_id={self.channel_id} topic={self.topic!r}>"
