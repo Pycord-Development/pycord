1b3d85810357df41d3a141d123b38056ccb7e9d4
Dorukyum <53639936+Dorukyum@users.noreply.github.com>
2022-07-28 20:10:42 +0300

    Update version_info

--- discord/__init__.py
+++ discord/__init__.py
@@ -76,6 +76,6 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=2, minor=0, micro=0, releaselevel="final", serial=0)
+version_info: VersionInfo = VersionInfo(major=2, minor=1, micro=0, releaselevel="final", serial=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
