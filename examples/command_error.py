b716e69c81d2ada030c232c4e1da3652ba62db5b
peco2282 <87461070+peco2282@users.noreply.github.com>
2022-07-31 16:53:36 +0900

    fix: suggestions

--- examples/command_error.py
+++ examples/command_error.py
@@ -13,15 +13,15 @@ bot = commands.Bot(
 )
 
 
-# return True if channel was thread.
-def is_thread(ctx: commands.Context) -> bool:
-    return isinstance(ctx.channel, discord.Thread)
-
-
 @bot.event
 async def on_ready():
     print(f"Logged in {bot.user.name} (ID: {bot.user.id})")
     print("------")
+
+
+# return True if channel was thread.
+def is_thread(ctx: commands.Context) -> bool:
+    return isinstance(ctx.channel, discord.Thread)
 
 
 @bot.command(name="archive")
@@ -29,30 +29,27 @@ async def on_ready():
 @commands.bot_has_permissions(manage_threads=True)
 @commands.check(is_thread)
 async def thread_archive(ctx: commands.Context):
-    # add success reaction.
     await ctx.message.add_reaction(positive_emoji)
-    # close Thread.
     await ctx.channel.archive()
 
 
 #  When `archive` command fails.
 @thread_archive.error
 async def archive_error(ctx: commands.Context, error: discord.DiscordException):
-    # React to that message.
     await ctx.message.add_reaction(negative_emoji)
 
     # Occurs when the bot does not have permission to manage threads.
-    # This error occurs `bot_has_permissions` decorator.
+    # This error occurs via the `bot_has_permissions` decorator.
     if isinstance(error, commands.BotMissingPermissions):
         return await ctx.send("Missing `manage_threads` permission")
 
     # Occurs when the channel is not a guild channel.
-    # This error occurs `guild_only` decorator.
+    # This error occurs via the `guild_only` decorator.
     if isinstance(error, commands.NoPrivateMessage):
         return await ctx.send("This command can use only guilds")
 
     # Occurs when is_thread function returns false. (the channel is not a thread)
-    # This error occurs `check(is_thread)` decorator.
+    # This error occurs via the `check(is_thread)` decorator, when `is_thread` returns false.
     # (occurs when the original check decorator is created and an error occurs there.)
     if isinstance(error, commands.CheckFailure):
         return await ctx.send("This channel is not thread!")
