c9e1734ce46f091e5589c7db0d922bce61815e5d
gigabit- <49173264+TheGiga@users.noreply.github.com>
2022-08-15 19:11:48 +0300

    Update wait_for_event.py
    Remade this example to slash command, because its more complicated in terms of functionallity. I think slash command fits better here

--- examples/wait_for_event.py
+++ examples/wait_for_event.py
@@ -1,48 +1,47 @@
-# This example requires the `message_content` privileged intent for access to message content.
-
-import asyncio
 import random
-
 import discord
-
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f"Logged in as {self.user} (ID: {self.user.id})")
-        print("------")
-
-    async def on_message(self, message: discord.Message):
-        # Make sure we won't be replying to ourselves.
-        if message.author.id == self.user.id:
-            return
-
-        if message.content.startswith("!guess"):
-            await message.channel.send("Guess a number between 1 and 10.")
-
-            def is_valid_guess(m: discord.Message):
-                # This function checks three things at once:
-                # - The author of the message we've received via
-                #   the wait_for is the same as the first message.
-                # - The content of the message is a digit.
-                # - The digit received is within the range of 1-10.
-                # If any one of these checks fail, we ignore this message.
-                return m.author == message.author and m.content.isdigit() and 1 <= int(m.content) <= 10
-
-            answer = random.randint(1, 10)
-
-            try:
-                guess = await self.wait_for("message", check=is_valid_guess, timeout=5.0)
-            except asyncio.TimeoutError:
-                return await message.channel.send(f"Sorry, you took too long it was {answer}.")
-
-            if int(guess.content) == answer:
-                await message.channel.send("You are right!")
-            else:
-                await message.channel.send(f"Oops. It is actually {answer}.")
-
+from asyncio import TimeoutError
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True  # < This may give you `read-only` warning, just ignore it.
+# This intent requires "Message Content Intent" to be enabled at https://discord.com/developers
 
-client = MyClient(intents=intents)
-client.run("TOKEN")
+
+bot = discord.Bot(intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print('Ready!')
+
+
+# For more examples regarding slash commands, checkout:
+# https://github.com/Pycord-Development/pycord/tree/master/examples/app_commands
+
+@bot.slash_command(name="guess", description="Guess a number between 1 and 10!")
+async def guess_number(ctx: discord.ApplicationContext):
+    await ctx.respond("Type in your number. *(It should be between 1 and 10)*")
+
+    def is_valid_guess(m: discord.Message):
+        # This function checks three things at once:
+        # - The author of the message we've received via
+        #   the wait_for is the same as the first message.
+        # - The content of the message is a digit.
+        # - The digit received is within the range of 1-10.
+        # If any one of these checks fail, we ignore this message.
+        return m.author == ctx.author and m.content.isdigit() and 1 <= int(m.content) <= 10
+
+    answer = random.randint(1, 10)
+
+    try:
+        guess: discord.Message = await bot.wait_for("message", check=is_valid_guess, timeout=5.0)
+    except TimeoutError:
+        return await ctx.send_followup(f"Sorry, you took too long it was {answer}.")
+
+    if int(guess.content) == answer:
+        await guess.reply("You are right!", mention_author=True)
+    else:
+        await guess.reply(f"Oops. It is actually {answer}.", mention_author=True)
+
+
+bot.run("TOKEN")
