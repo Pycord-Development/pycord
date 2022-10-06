3bfe7165f20192e32dd684d9b5856359634e35db
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2022-10-05 20:49:01 -0500

    Bump version to 2.2.1

--- discord/__init__.py
+++ discord/__init__.py
@@ -12,7 +12,7 @@ __title__ = "pycord"
 __author__ = "Pycord Development"
 __license__ = "MIT"
 __copyright__ = "Copyright 2015-2021 Rapptz & Copyright 2021-present Pycord Development"
-__version__ = "2.2.0"
+__version__ = "2.2.1"
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
@@ -76,7 +76,7 @@ class VersionInfo(NamedTuple):
 
 
 version_info: VersionInfo = VersionInfo(
-    major=2, minor=2, micro=0, releaselevel="final", serial=0
+    major=2, minor=2, micro=1, releaselevel="final", serial=0
 )
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
