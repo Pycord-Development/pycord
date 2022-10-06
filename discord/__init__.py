51b0fb8c4ce7eb164797ea0c4e9bbdfed19776ec
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2022-10-05 21:51:38 -0500

    Remove version variable

--- discord/__init__.py
+++ discord/__init__.py
@@ -12,7 +12,6 @@ __title__ = "pycord"
 __author__ = "Pycord Development"
 __license__ = "MIT"
 __copyright__ = "Copyright 2015-2021 Rapptz & Copyright 2021-present Pycord Development"
-__version__ = "2.2.2"
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
