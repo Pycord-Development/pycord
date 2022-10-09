d8e9c845046b20c7f1b133c4c524365087c4812f
NeloBlivion <41271523+NeloBlivion@users.noreply.github.com>
2022-10-09 19:12:46 +0100

    Minor get_or_fetch docs fix

--- discord/utils.py
+++ discord/utils.py
@@ -562,7 +562,8 @@ def get(iterable: Iterable[T], **attrs: Any) -> T | None:
 
 
 async def get_or_fetch(obj, attr: str, id: int, *, default: Any = MISSING) -> Any:
-    """This is a :ref:`coroutine <coroutine>`
+    """|coro|
+    
     Attempts to get an attribute from the object in cache. If it fails, it will attempt to fetch it.
     If the fetch also fails, an error will be raised.
 
