dbfb5a843ef213624a4344aa51e4dc4c2ac7052a
William Grigor <bjgrigor@gmail.com>
2022-10-11 22:50:01 -0500

    Implemented minimal working test runner

--- tests/parsing.py
+++ tests/parsing.py
@@ -1,10 +1,11 @@
 import json
+import logging
 from dataclasses import dataclass
 from typing import Any, Dict, Optional, Type, TypeVar, Union
 
 from proxy.http.parser import HttpParser
 
-T = TypeVar("T", bound="CombinedHttpRequest")
+T = TypeVar('T', bound='CombinedHttpRequest')
 
 
 @dataclass
@@ -12,23 +13,27 @@ class CombinedHttpRequest:
     headers: Dict[str, str]
     host: str
     path: str
+    full_url: str
     body: Optional[Union[str, bytes, Dict[str, Any]]]
 
     @classmethod
-    def from_request_pair(
-        cls: Type[T], connect_req: HttpParser, final_req: HttpParser
-    ) -> T:
+    def from_request_pair(cls: Type[T], connect_req: HttpParser, final_req: HttpParser) -> T:
+
         headers_merged = connect_req.headers | final_req.headers
-        parsed_headers = {
-            k.decode("utf-8"): v.decode("utf-8") for k, v in headers_merged.values()
-        }
-        host = connect_req.url.netloc.decode("utf-8").split(":")[0]
-        path = final_req.url.path.decode("utf-8")
-        content_type = parsed_headers["Content-Type"]
-        if content_type == "text/plain":
-            body = final_req.body.decode("utf-8")
-        elif content_type == "application/json":
+        parsed_headers = {k.decode('utf-8'): v.decode('utf-8')
+                          for k, v in headers_merged.values()}
+        logging.info(str(connect_req.type))
+        logging.info(str(final_req.protocol))
+        # scheme = connect_req._url.scheme.decode('utf-8')
+        # host = connect_req._url.netloc.decode('utf-8').split(':')[0]
+        host = connect_req.host.decode('utf-8')
+        path = final_req.path.decode('utf-8')
+        full_url = f'{host}{path}'
+        content_type = parsed_headers.get('Content-Type', None)
+
+        if content_type == 'application/json':
             body = json.loads(final_req.body)
         else:
-            body = final_req.body
-        return cls(headers=parsed_headers, host=host, body=body, path=path)
+            body = None
+
+        return cls(headers=parsed_headers, host=host, body=body, path=path, full_url=full_url)
