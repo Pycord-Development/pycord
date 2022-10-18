ec56c7b2be40d793d0f631b451442def656d129b
gigabit- <49173264+TheGiga@users.noreply.github.com>
2022-10-18 19:32:37 +0000

    back to typevar

--- discord/guild.py
+++ discord/guild.py
@@ -895,15 +895,18 @@ class Guild(Hashable):
         """
         return self._members.get(user_id)
 
-        _FETCHABLE = Union[
-            VoiceChannel,
-            TextChannel,
-            ForumChannel,
-            StageChannel,
-            CategoryChannel,
-            Thread,
-            Member,
-        ]
+    _FETCHABLE = TypeVar(
+            "_FETCHABLE",
+            bound=Union[
+                VoiceChannel,
+                TextChannel,
+                ForumChannel,
+                StageChannel,
+                CategoryChannel,
+                Thread,
+                Member,
+            ]
+    )
 
     async def get_or_fetch(
         self, object_type: Type[_FETCHABLE], object_id: int, /
