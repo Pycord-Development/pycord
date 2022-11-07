feceba5a2f895fa3bb4c50d4d3e851d9b8faad16
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2022-11-07 14:35:20 -0600

    Add __future__ annotations import

--- discord/ext/commands/help.py
+++ discord/ext/commands/help.py
@@ -23,6 +23,8 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
+from __future__ import annotations
+
 import copy
 import functools
 import itertools
