3f64daaf1e14b15a11cd1a851c8d008b434e7706
plun1331 <49261529+plun1331@users.noreply.github.com>
2022-12-09 15:50:06 -0800

    Update application_role_connection.py

--- discord/application_role_connection.py
+++ discord/application_role_connection.py
@@ -39,6 +39,8 @@ __all__ = ("ApplicationRoleConnectionMetadata",)
 
 class ApplicationRoleConnectionMetadata:
     r"""Represents role connection metadata for a Discord application.
+    
+    .. versionadded:: 2.4
 
     Parameters
     ----------
