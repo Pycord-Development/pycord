4179b3ba28dfcc1eeee96366e9fed7e17ec687b6
UK <41271523+NeloBlivion@users.noreply.github.com>
2023-01-27 20:33:53 +0000

    fix: Document Option.channel_types and properly support further channel types (#1883)
    * Add channel_types documentation for discord.Option
    
    * fix doc note
    
    * Support forum and private channels(?)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Clarification in docs
    
    * typo
    
    * Use new annotation syntax
    
    Co-authored-by: BobDotCom <71356958+BobDotCom@users.noreply.github.com>
    Signed-off-by: UK <41271523+NeloBlivion@users.noreply.github.com>
    
    * Specify more appropriate type
    
    Signed-off-by: UK <41271523+NeloBlivion@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: UK <41271523+NeloBlivion@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Lala Sabathil <lala@pycord.dev>
    Co-authored-by: plun1331 <49261529+plun1331@users.noreply.github.com>
    Co-authored-by: BobDotCom <71356958+BobDotCom@users.noreply.github.com>

--- discord/commands/options.py
+++ discord/commands/options.py
@@ -29,7 +29,15 @@ from enum import Enum
 from typing import TYPE_CHECKING, Literal, Optional, Type, Union
 
 from ..abc import GuildChannel, Mentionable
-from ..channel import CategoryChannel, StageChannel, TextChannel, Thread, VoiceChannel
+from ..channel import (
+    CategoryChannel,
+    DMChannel,
+    ForumChannel,
+    StageChannel,
+    TextChannel,
+    Thread,
+    VoiceChannel,
+)
 from ..enums import ChannelType
 from ..enums import Enum as DiscordEnum
 from ..enums import SlashCommandOptionType
@@ -73,6 +81,8 @@ CHANNEL_TYPE_MAP = {
     StageChannel: ChannelType.stage_voice,
     CategoryChannel: ChannelType.category,
     Thread: ChannelType.public_thread,
+    ForumChannel: ChannelType.forum,
+    DMChannel: ChannelType.private,
 }
 
 
@@ -138,6 +148,10 @@ class Option:
         .. note::
 
             Does not validate the input value against the autocomplete results.
+    channel_types: list[:class:`discord.ChannelType`] | None
+        A list of channel types that can be selected in this option.
+        Only applies to Options with an :attr:`input_type` of :class:`discord.SlashCommandOptionType.channel`.
+        If this argument is used, :attr:`input_type` will be ignored.
     name_localizations: Optional[Dict[:class:`str`, :class:`str`]]
         The name localizations for this option. The values of this should be ``"locale": "name"``.
         See `here <https://discord.com/developers/docs/reference#locales>`_ for a list of valid locales.
@@ -224,11 +238,12 @@ class Option:
                                 self._raw_type = input_type.__args__  # type: ignore # Union.__args__
                             else:
                                 self._raw_type = (input_type,)
-                        self.channel_types = [
-                            CHANNEL_TYPE_MAP[t]
-                            for t in self._raw_type
-                            if t is not GuildChannel
-                        ]
+                        if not self.channel_types:
+                            self.channel_types = [
+                                CHANNEL_TYPE_MAP[t]
+                                for t in self._raw_type
+                                if t is not GuildChannel
+                            ]
         self.required: bool = (
             kwargs.pop("required", True) if "default" not in kwargs else False
         )
