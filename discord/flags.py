1f1372f5184af645fe10bb3ad38ed9da64b62530
plun1331 <49261529+plun1331@users.noreply.github.com>
2023-03-28 21:57:28 -0700

    Update discord/flags.py
    Signed-off-by: plun1331 <49261529+plun1331@users.noreply.github.com>

--- discord/flags.py
+++ discord/flags.py
@@ -1340,7 +1340,7 @@ class ApplicationFlags(BaseFlags):
 
     @flag_value
     def application_auto_moderation_rule_create_badge(self):
-        """:class:`bool`: Returns ``True`` if the application uses the Auto Moderation API
+        """:class:`bool`: Returns ``True`` if the application uses the Auto Moderation API.
 
         .. versionadded:: 2.5
         """
