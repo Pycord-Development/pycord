7aa1386a50870fffb478e45b4c281153930d2f98
Om Lanke <oplanke@gmail.com>
2023-06-01 03:05:48 +0530

    cooldowns and stuff for `SlashCommandGroup`s

--- discord/commands/core.py
+++ discord/commands/core.py
@@ -294,18 +294,17 @@ class ApplicationCommand(_BaseCommand, Generic[CogT, P, T]):
                 f"The check functions for the command {self.name} failed"
             )
 
-        if hasattr(self, "_max_concurrency"):
-            if self._max_concurrency is not None:
-                # For this application, context can be duck-typed as a Message
-                await self._max_concurrency.acquire(ctx)  # type: ignore # ctx instead of non-existent message
+        if self._max_concurrency is not None:
+            # For this application, context can be duck-typed as a Message
+            await self._max_concurrency.acquire(ctx)  # type: ignore # ctx instead of non-existent message
 
-            try:
-                self._prepare_cooldowns(ctx)
-                await self.call_before_hooks(ctx)
-            except:
-                if self._max_concurrency is not None:
-                    await self._max_concurrency.release(ctx)  # type: ignore # ctx instead of non-existent message
-                raise
+        try:
+            self._prepare_cooldowns(ctx)
+            await self.call_before_hooks(ctx)
+        except:
+            if self._max_concurrency is not None:
+                await self._max_concurrency.release(ctx)  # type: ignore # ctx instead of non-existent message
+            raise
 
     def is_on_cooldown(self, ctx: ApplicationContext) -> bool:
         """Checks whether the command is currently on cooldown.
@@ -1152,6 +1151,28 @@ class SlashCommandGroup(ApplicationCommand):
         self.description_localizations: dict[str, str] = kwargs.get(
             "description_localizations", MISSING
         )
+
+        # exactly like ApplicationCommand
+        from ..ext.commands.cooldowns import BucketType, CooldownMapping, MaxConcurrency
+
+        cooldown = getattr(self, "__commands_cooldown__", kwargs.get("cooldown"))
+
+        if cooldown is None:
+            buckets = CooldownMapping(cooldown, BucketType.default)
+        elif isinstance(cooldown, CooldownMapping):
+            buckets = cooldown
+        else:
+            raise TypeError(
+                "Cooldown must be a an instance of CooldownMapping or None."
+            )
+
+        self._buckets: CooldownMapping = buckets
+
+        max_concurrency = getattr(
+            self, "__commands_max_concurrency__", kwargs.get("max_concurrency")
+        )
+
+        self._max_concurrency: MaxConcurrency | None = max_concurrency
 
     @property
     def module(self) -> str | None:
