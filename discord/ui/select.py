6aac42a044e5b13e610e08f02a03914dd92d8c86
plun1331 <49261529+plun1331@users.noreply.github.com>
2023-06-08 16:59:19 -0700

    feat: return None instead of an empty list
    Signed-off-by: plun1331 <49261529+plun1331@users.noreply.github.com>

--- discord/ui/select.py
+++ discord/ui/select.py
@@ -335,15 +335,15 @@ class Select(Item[V]):
         | list[GuildChannel | Thread]
     ):
         """List[:class:`str`] | List[:class:`discord.Member` | :class:`discord.User`]] | List[:class:`discord.Role`]] |
-        List[:class:`discord.Member` | :class:`discord.User` | :class:`discord.Role`]] | List[:class:`discord.abc.GuildChannel`]:
-        A list of values that have been selected by the user. This will be an empty list if the select has not been interacted with yet.
+        List[:class:`discord.Member` | :class:`discord.User` | :class:`discord.Role`]] | List[:class:`discord.abc.GuildChannel`] | None:
+        A list of values that have been selected by the user. This will be ``None`` if the select has not been interacted with yet.
         """
+        if self._interaction is None:
+            # The select has not been interacted with yet
+            return None
         select_type = self._underlying.type
         if select_type is ComponentType.string_select:
             return self._selected_values
-        if self._interaction is None:
-            # The select has not been interacted with yet
-            return []
         resolved = []
         selected_values = list(self._selected_values)
         state = self._interaction._state
