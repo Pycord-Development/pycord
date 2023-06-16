Pycord-Development/pycord

commit d894a586e1660ed50692880f77c8097a16de552f

feat: handle typing.Annotated form annotations in slash commands option parser. (#2124)
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Lala Sabathil <lala@pycord.dev>

discord/commands/core.py
@@ -44,6 +44,8 @@ from typing import (
     Union,
 )
 
+from typing_extensions import Annotated, get_args, get_origin
+
 from ..channel import _threaded_guild_channel_factory
 from ..enums import Enum as DiscordEnum
 from ..enums import MessageType, SlashCommandOptionType, try_enum
@@ -732,6 +734,19 @@ class SlashCommand(ApplicationCommand):
             if option == inspect.Parameter.empty:
                 option = str
 
+            if self._is_typing_annotated(option):
+                type_hint = get_args(option)[0]
+                metadata = option.__metadata__
+                # If multiple Options in metadata, the first will be used.
+                option_gen = (elem for elem in metadata if isinstance(elem, Option))
+                option = next(option_gen, Option())
+                # Handle Optional
+                if self._is_typing_optional(type_hint):
+                    option.input_type = get_args(type_hint)[0]
+                    option.default = None
+                else:
+                    option.input_type = type_hint
+
             if self._is_typing_union(option):
                 if self._is_typing_optional(option):
                     option = Option(option.__args__[0], default=None)
@@ -819,6 +834,9 @@ class SlashCommand(ApplicationCommand):
 
     def _is_typing_optional(self, annotation):
         return self._is_typing_union(annotation) and type(None) in annotation.__args__  # type: ignore
+
+    def _is_typing_annotated(self, annotation):
+        return get_origin(annotation) is Annotated
 
     @property
     def cog(self):

tests/test_typing_annotated.py
@@ -1,0 +1,86 @@
+from typing import Optional
+
+import pytest
+from typing_extensions import Annotated
+
+import discord
+from discord import ApplicationContext
+from discord.commands.core import SlashCommand, slash_command
+
+
+def test_typing_annotated():
+    async def echo(ctx, txt: Annotated[str, discord.Option()]):
+        await ctx.respond(txt)
+
+    cmd = SlashCommand(echo)
+    bot = discord.Bot()
+    bot.add_application_command(cmd)
+
+
+def test_typing_annotated_decorator():
+    bot = discord.Bot()
+
+    @bot.slash_command()
+    async def echo(ctx, txt: Annotated[str, discord.Option(description="Some text")]):
+        await ctx.respond(txt)
+
+
+def test_typing_annotated_cog():
+    class echoCog(discord.Cog):
+        def __init__(self, bot_) -> None:
+            self.bot = bot_
+            super().__init__()
+
+        @slash_command()
+        async def echo(
+            self, ctx, txt: Annotated[str, discord.Option(description="Some text")]
+        ):
+            await ctx.respond(txt)
+
+    bot = discord.Bot()
+    bot.add_cog(echoCog(bot))
+
+
+def test_typing_annotated_cog_slashgroup():
+    class echoCog(discord.Cog):
+        grp = discord.commands.SlashCommandGroup("echo")
+
+        def __init__(self, bot_) -> None:
+            self.bot = bot_
+            super().__init__()
+
+        @grp.command()
+        async def echo(
+            self, ctx, txt: Annotated[str, discord.Option(description="Some text")]
+        ):
+            await ctx.respond(txt)
+
+    bot = discord.Bot()
+    bot.add_cog(echoCog(bot))
+
+
+def test_typing_annotated_optional():
+    async def echo(ctx, txt: Annotated[Optional[str], discord.Option()]):
+        await ctx.respond(txt)
+
+    cmd = SlashCommand(echo)
+    bot = discord.Bot()
+    bot.add_application_command(cmd)
+
+
+def test_no_annotation():
+    async def echo(ctx, txt: str):
+        await ctx.respond(txt)
+
+    cmd = SlashCommand(echo)
+    bot = discord.Bot()
+    bot.add_application_command(cmd)
+
+
+def test_annotated_no_option():
+    async def echo(ctx, txt: Annotated[str, "..."]):
+        await ctx.respond(txt)
+
+    cmd = SlashCommand(echo)
+    bot = discord.Bot()
+    bot.add_application_command(cmd)

commit a6ef8ebfd8574541d33f2a75a0032d36e7817066

fix: Correct regression from pull #2124
Pycord requires typing_extensions as a dependency for python < 3.11,
however not on version >= 3.11. Because the changes in the referenced
pull always tries to load from typing_extensions, they make it so an
exception will be raised if the end user is using python >= 3.11 and
does not otherwise have that library installed when _parse_options is
 run (ie, whenever a command is registered).

This change checks the version of python running Pycord, then imports
stdlib typing if >= 3.11.

discord/commands/core.py
@@ -30,6 +30,7 @@ import datetime
 import functools
 import inspect
 import re
+import sys
 import types
 from collections import OrderedDict
 from enum import Enum
@@ -43,8 +44,6 @@ from typing import (
     TypeVar,
     Union,
 )
-
-from typing_extensions import Annotated, get_args, get_origin
 
 from ..channel import _threaded_guild_channel_factory
 from ..enums import Enum as DiscordEnum
@@ -65,6 +64,11 @@ from ..user import User
 from ..utils import MISSING, async_all, find, maybe_coroutine, utcnow
 from .context import ApplicationContext, AutocompleteContext
 from .options import Option, OptionChoice
+
+if sys.version_info >= (3, 11):
+    from typing import Annotated, get_args, get_origin
+else:
+    from typing_extensions import Annotated, get_args, get_origin
 
 __all__ = (
     "_BaseCommand",