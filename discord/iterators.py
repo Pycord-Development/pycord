520d003cc1274158e5089dd1437f6a7c2c01e17e
Honza Javorek <mail@honzajavorek.cz>
2024-01-30 11:07:42 +0100

    add types
    Signed-off-by: Honza Javorek <mail@honzajavorek.cz>

--- discord/iterators.py
+++ discord/iterators.py
@@ -335,7 +335,7 @@ class HistoryIterator(_AsyncIterator["Message"]):
         except asyncio.QueueEmpty:
             raise NoMoreItems()
 
-    def _get_retrieve(self):
+    def _get_retrieve(self) -> bool:
         l = self.limit
         if l is None or l > 100:
             r = 100
@@ -366,11 +366,11 @@ class HistoryIterator(_AsyncIterator["Message"]):
                     self.state.create_message(channel=channel, data=element)
                 )
 
-    async def _retrieve_messages(self, retrieve) -> list[Message]:
+    async def _retrieve_messages(self, retrieve: int) -> list[MessagePayload]:
         """Retrieve messages and update next parameters."""
         raise NotImplementedError
 
-    async def _retrieve_messages_before_strategy(self, retrieve):
+    async def _retrieve_messages_before_strategy(self, retrieve: int) -> list[MessagePayload]:
         """Retrieve messages using before parameter."""
         before = self.before.id if self.before else None
         data: list[MessagePayload] = await self.logs_from(
@@ -382,7 +382,7 @@ class HistoryIterator(_AsyncIterator["Message"]):
             self.before = Object(id=int(data[-1]["id"]))
         return data
 
-    async def _retrieve_messages_after_strategy(self, retrieve):
+    async def _retrieve_messages_after_strategy(self, retrieve: int) -> list[MessagePayload]:
         """Retrieve messages using after parameter."""
         after = self.after.id if self.after else None
         data: list[MessagePayload] = await self.logs_from(
@@ -394,7 +394,7 @@ class HistoryIterator(_AsyncIterator["Message"]):
             self.after = Object(id=int(data[0]["id"]))
         return data
 
-    async def _retrieve_messages_around_strategy(self, retrieve):
+    async def _retrieve_messages_around_strategy(self, retrieve: int) -> list[MessagePayload]:
         """Retrieve messages using around parameter."""
         if self.around:
             around = self.around.id if self.around else None
