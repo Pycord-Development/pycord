d179f699b9d538003590d9bdef64d12ef7adde2a
plun1331 <plun1331@gmail.com>
2024-05-16 13:06:41 -0700

    Update components.py

--- discord/types/components.py
+++ discord/types/components.py
@@ -30,6 +30,7 @@ from typing import Literal, Union
 from .._typed_dict import NotRequired, TypedDict
 from .channel import ChannelType
 from .emoji import PartialEmoji
+from .snowflake import Snowflake
 
 ComponentType = Literal[1, 2, 3, 4]
 ButtonStyle = Literal[1, 2, 3, 4, 5]
@@ -49,6 +50,7 @@ class ButtonComponent(TypedDict):
     label: NotRequired[str]
     type: Literal[2]
     style: ButtonStyle
+    sku_id: Snowflake
 
 
 class InputText(TypedDict):
