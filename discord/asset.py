3aeee34e931d00818e3aae20c73663800c777063
Matty Widdop <18513864+MattyTheHacker@users.noreply.github.com>
2024-08-22 13:07:34 +0100

    fix: set size to `1024` for `_from_user_banner`
    Signed-off-by: Matty Widdop <18513864+MattyTheHacker@users.noreply.github.com>

--- discord/asset.py
+++ discord/asset.py
@@ -284,7 +284,7 @@ class Asset(AssetMixin):
         format = "gif" if animated else "png"
         return cls(
             state,
-            url=f"{cls.BASE}/banners/{user_id}/{banner_hash}.{format}?size=512",
+            url=f"{cls.BASE}/banners/{user_id}/{banner_hash}.{format}?size=1024",
             key=banner_hash,
             animated=animated,
         )
