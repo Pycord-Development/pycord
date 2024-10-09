Pycord-Development/pycord

commit b25d9a7915a04629bb12c522082b586207ade26b

Update discord/ext/commands/cooldowns.py
Co-authored-by: Dorukyum <53639936+Dorukyum@users.noreply.github.com>
Signed-off-by: Revnoplex <62947003+Revnoplex@users.noreply.github.com>

discord/ext/commands/cooldowns.py
@@ -73,6 +73,7 @@ class BucketType(Enum):
             return (
                 msg.channel.category.id
                 if isinstance(msg.channel, discord.abc.GuildChannel)
+                and channel.category
                 and msg.channel.category
                 else msg.channel.id
             )

commit 67a8939a0dbcc0845889140c0c3c612ed25c0254

undo broken merge caused by b25d9a7
Signed-off-by: Revnoplex <62947003+Revnoplex@users.noreply.github.com>

discord/ext/commands/cooldowns.py
@@ -73,7 +73,6 @@ class BucketType(Enum):
             return (
                 msg.channel.category.id
                 if isinstance(msg.channel, discord.abc.GuildChannel)
-                and channel.category
                 and msg.channel.category
                 else msg.channel.id
             )