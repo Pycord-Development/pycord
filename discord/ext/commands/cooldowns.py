Pycord-Development/pycord

commit b25d9a7915a04629bb12c522082b586207ade26b

Update discord/ext/commands/cooldowns.py
Co-authored-by: Dorukyum <53639936+Dorukyum@users.noreply.github.com>
Signed-off-by: Revnoplex <62947003+Revnoplex@users.noreply.github.com>

discord/ext/commands/cooldowns.py
@@ -73,6 +73,7 @@ class BucketType(Enum):
             return (
                 msg.channel.category.id
                 if isinstance(msg.channel, discord.abc.GuildChannel)
+                and channel.category
                 and msg.channel.category
                 else msg.channel.id
             )

commit c23d8fc43aeaf9ac2ab9148884b7a1971fb7d8e0

fix: category cooldown commands works in dms like other guild cooldowns (#2603)
* fix: category cooldown works in dms like other guild cooldowns

* style(pre-commit): auto fixes from pre-commit.com hooks

* Add changelog entry

* style(pre-commit): auto fixes from pre-commit.com hooks

* Update discord/ext/commands/cooldowns.py

Co-authored-by: Dorukyum <53639936+Dorukyum@users.noreply.github.com>
Signed-off-by: Revnoplex <62947003+Revnoplex@users.noreply.github.com>

* Update discord/ext/commands/cooldowns.py

Co-authored-by: Dorukyum <53639936+Dorukyum@users.noreply.github.com>
Signed-off-by: Revnoplex <62947003+Revnoplex@users.noreply.github.com>

* fix mistyped suggestion cooldowns.py

Signed-off-by: Revnoplex <62947003+Revnoplex@users.noreply.github.com>

* Update discord/ext/commands/cooldowns.py

Co-authored-by: Dorukyum <53639936+Dorukyum@users.noreply.github.com>
Signed-off-by: Revnoplex <62947003+Revnoplex@users.noreply.github.com>

* undo broken merge caused by b25d9a7

Signed-off-by: Revnoplex <62947003+Revnoplex@users.noreply.github.com>

* Update CHANGELOG.md

Signed-off-by: Dorukyum <53639936+Dorukyum@users.noreply.github.com>

---------

Signed-off-by: Revnoplex <62947003+Revnoplex@users.noreply.github.com>
Signed-off-by: Dorukyum <53639936+Dorukyum@users.noreply.github.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Dorukyum <53639936+Dorukyum@users.noreply.github.com>

discord/ext/commands/cooldowns.py
@@ -30,6 +30,7 @@ import time
 from collections import deque
 from typing import TYPE_CHECKING, Any, Callable, Deque, TypeVar
 
+import discord.abc
 from discord.enums import Enum
 
 from ...abc import PrivateChannel
@@ -69,7 +70,12 @@ class BucketType(Enum):
         elif self is BucketType.member:
             return (msg.guild and msg.guild.id), msg.author.id
         elif self is BucketType.category:
-            return (msg.channel.category or msg.channel).id  # type: ignore
+            return (
+                msg.channel.category.id
+                if isinstance(msg.channel, discord.abc.GuildChannel)
+                and msg.channel.category
+                else msg.channel.id
+            )
         elif self is BucketType.role:
             # we return the channel id of a private-channel as there are only roles in guilds
             # and that yields the same result as for a guild with only the @everyone role