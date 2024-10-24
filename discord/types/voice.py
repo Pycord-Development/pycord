77f56833078019002ca4b9ba0fd0845f00dc331a
Paillat <me@paillat.dev>
2024-10-24 14:41:51 +0200

    :recycle: Use `typing_extensions` instead of `_typed_dict`

--- discord/types/voice.py
+++ discord/types/voice.py
@@ -27,7 +27,8 @@ from __future__ import annotations
 
 from typing import Literal
 
-from .._typed_dict import NotRequired, TypedDict
+from typing_extensions import NotRequired, TypedDict
+
 from .member import MemberWithUser
 from .snowflake import Snowflake
 
