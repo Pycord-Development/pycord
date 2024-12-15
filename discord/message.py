228c1221a258b99c31db70469506b9283bb88880
Timo <35654063+tibue99@users.noreply.github.com>
2024-12-15 14:08:24 +0100

    Add message.to_dict

--- discord/message.py
+++ discord/message.py
@@ -789,6 +789,7 @@ class Message(Hashable):
 
     __slots__ = (
         "_state",
+        "_data",
         "_edited_timestamp",
         "_cs_channel_mentions",
         "_cs_raw_mentions",
@@ -842,6 +843,7 @@ class Message(Hashable):
         data: MessagePayload,
     ):
         self._state: ConnectionState = state
+        self._data: MessagePayload = data
         self.id: int = int(data["id"])
         self.webhook_id: int | None = utils._get_as_snowflake(data, "webhook_id")
         self.reactions: list[Reaction] = [
@@ -1990,6 +1992,9 @@ class Message(Hashable):
             data["guild_id"] = self.guild.id
 
         return data
+
+    def to_dict(self) -> MessagePayload:
+        return self._data
 
 
 class PartialMessage(Hashable):
