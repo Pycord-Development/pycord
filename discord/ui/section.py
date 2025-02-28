d1a3711957877129293192fd817f1bff8861afa4
UK <41271523+NeloBlivion@users.noreply.github.com>
2025-02-28 23:41:16 +0000

    imports

--- discord/ui/section.py
+++ discord/ui/section.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, TypeVar
+from typing import TYPE_CHECKING, TypeVar, ClassVar
+from functools import partial
 
 from ..components import Section as SectionComponent
 from ..components import _component_factory
