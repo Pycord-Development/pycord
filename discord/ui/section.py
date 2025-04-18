b63e224d772c11e7ca8c6170d7d33ef33c96af26
UK <41271523+NeloBlivion@users.noreply.github.com>
2025-04-18 19:28:15 +0100

    minor fix

--- discord/ui/section.py
+++ discord/ui/section.py
@@ -182,7 +182,7 @@ class Section(Item[V]):
 
     def copy_text(self) -> str:
         """Returns the text of all :class:`~discord.ui.TextDisplay` items in this section. Equivalent to the `Copy Text` option on Discord clients."""
-        return "\n".join(i.text for i in self.items)
+        return "\n".join(i.copy_text() for i in self.items)
 
     @property
     def type(self) -> ComponentType:
