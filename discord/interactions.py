5aace18c6b5595b82f4b4f1d434d27cb34333e19
Ice Wolfy <icebluewolf05@gmail.com>
2025-09-03 10:00:09 -0400

    feat: Add __repr__ to InteractionCallback

--- discord/interactions.py
+++ discord/interactions.py
@@ -1770,6 +1770,11 @@ class InteractionCallback:
             "response_message_ephemeral", False
         )
 
+    def __repr__(self):
+        return (f"<InteractionCallback "
+                f"_response_message_loading={self._response_message_loading} "
+                f"_response_message_ephemeral={self._response_message_ephemeral}>")
+
     def is_loading(self) -> bool:
         """Indicates whether the response message is in a loading state."""
         return self._response_message_loading
