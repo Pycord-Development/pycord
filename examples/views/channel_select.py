9c552968e12efd1853afc2f6dac7df79ca30f0a0
DA-344 <108473820+DA-344@users.noreply.github.com>
2025-09-10 16:27:12 +0200

    use default typings for Select in examples

--- examples/views/channel_select.py
+++ examples/views/channel_select.py
@@ -11,7 +11,7 @@ class DropdownView(discord.ui.View):
         placeholder="Select channels...", min_values=1, max_values=3
     )  # Users can select a maximum of 3 channels in the dropdown
     async def channel_select_dropdown(
-        self, select: discord.ui.Select, interaction: discord.Interaction
+        self, select: discord.ui.ChannelSelect, interaction: discord.Interaction
     ) -> None:
         # update the select default values to the chosen values
         select.default_values = select.values  # this is a list of GuildChannels
