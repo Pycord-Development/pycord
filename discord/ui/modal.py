6eb6086df779c5398c560352dda4b85b39aff5b6
Nelo <41271523+NeloBlivion@users.noreply.github.com>
2025-12-25 05:20:21 -0500

    Iterator,

--- discord/ui/modal.py
+++ discord/ui/modal.py
@@ -30,7 +30,7 @@ import sys
 import time
 from functools import partial
 from itertools import groupby
-from typing import TYPE_CHECKING, Any, TypeVar
+from typing import TYPE_CHECKING, Any, TypeVar, Iterator
 
 from ..enums import ComponentType
 from ..utils import find
