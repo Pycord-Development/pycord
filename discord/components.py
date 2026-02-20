301fc093058b38400c697aac9124a5d29fea2556
Evanroby <107794516+Evanroby@users.noreply.github.com>
2026-02-20 10:22:10 +0100

    Revert "make FileUpload required optional"
    This reverts commit 1f09a095214a53a5216ea4a892944f5d55525279.

--- discord/components.py
+++ discord/components.py
@@ -1397,7 +1397,7 @@ class FileUpload(Component):
         self.custom_id = data["custom_id"]
         self.min_values: int | None = data.get("min_values", None)
         self.max_values: int | None = data.get("max_values", None)
-        self.required: bool | None = data.get("required")
+        self.required: bool = data.get("required", True)
 
     def to_dict(self) -> FileUploadComponentPayload:
         payload = {
@@ -1413,7 +1413,7 @@ class FileUpload(Component):
         if self.max_values is not None:
             payload["max_values"] = self.max_values
 
-        if self.required is not None:
+        if not self.required:
             payload["required"] = self.required
 
         return payload  # type: ignore
