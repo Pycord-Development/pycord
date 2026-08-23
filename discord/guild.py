c525950919a71169604cb0da4f65e1c76f680f48
Dorukyum <53639936+Dorukyum@users.noreply.github.com>
2026-08-23 14:57:32 +0300

    remove versionchanged
    Signed-off-by: Dorukyum <53639936+Dorukyum@users.noreply.github.com>

--- discord/guild.py
+++ discord/guild.py
@@ -2823,9 +2823,6 @@ class Guild(Hashable):
         ``PRUNE_REQUIRES_ADMIN`` guild feature, you must have the
         :attr:`~Permissions.administrator` permission instead.
 
-        .. versionchanged:: 2.9
-            The ``PRUNE_REQUIRES_ADMIN`` guild feature requirement was added.
-
         Parameters
         ----------
         days: :class:`int`
